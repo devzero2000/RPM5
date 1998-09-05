b2d9daae551b85ad47add6d4759e75201f1278e4
jbj <jbj>
1998-09-05 14:13:42 +0000

    permit autogen.sh to work on non "prefix=/usr" hosts. CVm: ----------------------------------------------------------------------

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,7 @@ if [ "$1" = "--noconfigure" ]; then
     exit 0;
 fi
 
-if [ -z "$@" ]; then
+if [ X"$@" = X  -a "X`uname -s`" = "XLinux" ]; then
     ./configure --prefix=/usr
 else
     ./configure "$@"
