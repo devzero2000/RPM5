4ff31c66cab5cef1c13cbe2fdc09d63e0f4aa516
ewt <ewt>
2001-02-23 16:34:42 +0000

    plugged memory leak

--- python/rpmmodule.c
+++ python/rpmmodule.c
@@ -1973,8 +1973,7 @@ static int rpmMergeHeaders(PyObject * list, FD_t fd, int matchTag) {
 	    /* could be dupes */
 	    headerRemoveEntry(ho->h, tag);
 	    headerAddEntry(ho->h, tag, type, p, c);
-
-	    /* EWT we ened to free this item */
+	    headerFreeData(p, type);
 	}
 
 	headerFreeIterator(iter);
