cf3b7b063e42a19aec588ed6222826c43e1d3a8e
jbj <jbj>
2003-01-18 15:54:02 +0000

    Build with latest libtool/autoconf/automake. Add --enable-posixmutexes to db3 build if /usr/lib/nptl is present. Link dynamically to use posix mutexes.

--- autogen.sh
+++ autogen.sh
@@ -3,13 +3,13 @@
 export CFLAGS
 export LDFLAGS
 
-LTV="libtoolize (GNU libtool) 1.4.2"
-ACV="autoconf (GNU Autoconf) 2.54"
+LTV="libtoolize (GNU libtool) 1.4.3"
+ACV="autoconf (GNU Autoconf) 2.57"
 AMV="automake (GNU automake) 1.6.3"
 USAGE="
 This script documents the versions of the tools I'm using to build rpm:
-	libtool-1.4.2
-	autoconf-2.54
+	libtool-1.4.3
+	autoconf-2.57
 	automake-1.6.3
 Simply edit this script to change the libtool/autoconf/automake versions
 checked if you need to, as rpm should build (and has built) with all
@@ -23,7 +23,7 @@ recent versions of libtool/autoconf/automake.
 (echo "--- popt"; cd popt; ./autogen.sh --noconfigure "$@")
 (echo "--- zlib"; cd zlib; ./autogen.sh --noconfigure "$@")
 (echo "--- beecrypt"; cd beecrypt; ./autogen.sh --noconfigure "$@")
-(echo "--- libelf"; cd libelf; ./autogen.sh --noconfigure "$@")
+#(echo "--- libelf"; cd libelf; ./autogen.sh --noconfigure "$@")
 
 echo "--- rpm"
 libtoolize --copy --force
@@ -44,7 +44,12 @@ if [ X"$@" = X  -a "X`uname -s`" = "XLinux" ]; then
 	mandir=/usr/man
 	infodir=/usr/info
     fi
-    ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --infodir=${infodir} --mandir=${mandir} --without-javaglue "$@"
+    if [ -d /usr/lib/nptl ]; then
+	enable_posixmutexes="--enable-posixmutexes"
+    else
+	enable_posixmutexes=
+    fi
+    ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --infodir=${infodir} --mandir=${mandir} --without-javaglue ${enable_posixmutexes} "$@"
 else
     ./configure "$@"
 fi
