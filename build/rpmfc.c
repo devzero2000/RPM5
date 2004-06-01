2eea80988296570a26b874e59f38fdbd849b9d7b
jbj <jbj>
2004-06-01 16:48:22 +0000

    - fix: PIE executables have basename-as-soname provides (#123697).

--- build/rpmfc.c
+++ build/rpmfc.c
@@ -763,6 +763,7 @@ static int rpmfcELF(rpmfc fc)
     int isElf64;
     int isDSO;
     int gotSONAME = 0;
+    int gotDEBUG = 0;
     static int filter_GLIBC_PRIVATE = 0;
     static int oneshot = 0;
 
@@ -935,6 +936,9 @@ static int rpmfcELF(rpmfc fc)
 		    default:
 			/*@innercontinue@*/ continue;
 			/*@notreached@*/ /*@switchbreak@*/ break;
+                    case DT_DEBUG:    
+			gotDEBUG = 1;
+			/*@innercontinue@*/ continue;
 		    case DT_NEEDED:
 			/* Files with executable bit set only. */
 			if (fc->skipReq || !(st->st_mode & (S_IXUSR|S_IXGRP|S_IXOTH)))
@@ -989,7 +993,7 @@ assert(s != NULL);
     /*@=branchstate =uniondef @*/
 
     /* For DSO's, provide the basename of the file if DT_SONAME not found. */
-    if (!fc->skipProv && isDSO && !gotSONAME) {
+    if (!fc->skipProv && isDSO && !gotDEBUG && !gotSONAME) {
 	depsp = &fc->provides;
 	tagN = RPMTAG_PROVIDENAME;
 	dsContext = RPMSENSE_FIND_PROVIDES;
@@ -1296,7 +1300,8 @@ static DepMsg_t DepMsgs = depMsgs;
 /**
  */
 static void printDeps(Header h)
-	/*@modifies h @*/
+	/*@globals rpmGlobalMacroContext, h_errno, fileSystem, internalState @*/
+	/*@modifies h, rpmGlobalMacroContext, fileSystem, internalState @*/
 {
     DepMsg_t dm;
     rpmds ds = NULL;
