42a50b3b1017ba72558438e6c170533a91fb4af8
jbj <jbj>
2004-11-09 19:37:11 +0000

    - resurrect freshen.sh to, say, upgrade only kernel-bigmem packages.

--- scripts/freshen.sh
+++ scripts/freshen.sh
@@ -1,52 +1,82 @@
 #!/bin/sh
 
-# Implements --freshen option in RPM. --freshen is mostly like upgrade, but
-# go through each file and make sure the package is actually installed before
-# upgrading it. This won't work properly if there are any odd options
-# specified (i.e. filenames with " specified). I'm sure some shell-hacker
-# out there can fix that <hint, hint>.
+# Traditionally, and to some extent still, rpm --freshen upgraded
+# packages that matched by RPMTAG_NAME, not RPMTAG_PROVIDENAME.
+#
+# This freshen.sh script illustrates how to revert to the "traditional"
+# behavior for doing, say,
+#	rpm -Fvh kernel-bigmem*.rpm
+# so that only kernel-bigmem packages are upgraded, rather than
+# upgrading (i.e. erasing) every kernel package that contains
+#	Provides: kernel = V-R
+#
+# Copy the freshen.sh script to /usr/lib/rpm, and add the following
+# lines to /etc/popt to enable the behavior:
+#	rpm alias -F		--freshen
+#	rpm exec --freshen	/usr/lib/rpm/freshen.sh
+#	
 
-case `basename $0` in
-rpme|rpmi|rpmu)	rpm="$0" ;;
-*)		rpm="`dirname $0`/rpmi" ;;
-esac
-shift
+dbg=	#echo	# Do "dbg=echo" for debugging
+#set -x
+#echo "args: $*"
 
-args="-U"
+# Invoke rpmi from the same directory as freshen.sh.
+rpmi="`dirname $0`/rpmi"
+rpme="`dirname $0`/rpme"
+rpmq="`dirname $0`/rpmq"
+
+# Parse out any options and add to new arglist.
+# Note: this fails for options with arguments,
+# and doesn't detect multiple -i/-e/-U/-F options either.
+opts=""
 while [ $# -gt 0 ]; do
-    if [ "$1" = "--" ]; then
-	break
-    fi
-    args="$args $1"
-    shift
+    case $1 in
+    -*) opt="$1"
+	opts="$opts $opt" && shift
+	[ "$opt" = "--" ] && break
+	;;
+    *)	opts="$opts --" && break
+	;;
+    esac
+done
+#echo "opts: $opts"
+
+# $opts has the options with final '--', $* has the package files
+
+# If no remaining options, just invoke rpm (which will fail).
+[ $# = 0 ] && $dbg exec $rpmi -F $opts
+
+# Split remaining args into erase/install/upgrade invocations
+iargs=
+eargs=
+Fargs=
+for fn in $*; do
+    # If not a file, just pass to freshen.
+    [ ! -f $fn ] && Fargs="$Fargs $fn" && continue
+
+    # For all occurences of identically named packages installed ...
+    N="`$rpmq -qp --qf '%{NAME}' $fn`"
+    NVR="`$rpmq -qa $N`"
+
+    # ... special case kernel packages, ignore packages not installed.
+    case $N in
+    kernel*)
+	# ... if none installed, skip thi kernel package.
+	[ "$NVR" = "" ] && continue
+
+	# ... else install new package before erasing old package(s).
+	iargs="$iargs $fn"
+	eargs="$eargs $NVR"
+	;;
+    *) Fargs="$Fargs $fn";;	
+    esac
+
 done
 
-if [ $# = 0 ]; then
-    exec $rpm $args
-fi
+set -e		# Exit on any error from here on out.
 
-origargs="$args"
-args="$args -- "
-shift
-
-# Just filenames left now
-for n in $*; do
-    # if the file doesn't exist, we'll let RPM give the error message
-    if [ ! -f $n ]; then
-	args="$args $n"
-    else
-	if rpm -q `rpm -qp $n` >/dev/null 2>&1
-	then
-	    :
-	else
-	    name=`$rpm --qf "%{NAME}" -qp $n`
-	    $rpm -q $name >/dev/null 2>&1 && args="$args $n"
-	fi
-    fi
-done
-
-if [ "$args" = "$origargs -- " ]; then
-    echo no packages require freshening
-else
-    exec $rpm $args
-fi
+# Install before erase to insure deps are provided.
+[ "$iargs" != "" ] && $dbg $rpmi -i $opts $iargs
+[ "$eargs" != "" ] && $dbg $rpme -e $opts $eargs
+# Other, non-kernel, files passed to --freshen as always.
+[ "$Fargs" != "" ] && $dbg $rpmi -F $opts $Fargs
