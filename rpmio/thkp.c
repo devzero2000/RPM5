eaf1db63b601b326604b86ef4e82dec654621895
jbj <jbj>
2004-11-27 14:10:01 +0000

    Grab multiple pubkeys from key server.

--- rpmio/thkp.c
+++ rpmio/thkp.c
@@ -8,38 +8,75 @@
 #include "debug.h"
 
 static int _debug = 0;
+static int _printing = 1;
 
 extern int noNeon;
 
-#define	HTTPSPATH	"https://localhost/test.txt"
-#define	HTTPPATH	"http://localhost/test.txt"
+#if 0
 #define	HKPPATH		"hkp://pgp.mit.edu:11371/pks/lookup?op=get&search=0xF5C75256"
-#define	FTPPATH		"ftp://localhost/test.txt"
-#define	DIRPATH		"/var/ftp/test.txt"
-static char * httpspath = HTTPSPATH;
-static char * httppath = HTTPPATH;
+#else
+#define	HKPPATH		"hkp://pgp.mit.edu"
+#endif
 static char * hkppath = HKPPATH;
-static char * ftppath = FTPPATH;
-static char * dirpath = DIRPATH;
 
-static void readKey(const char * path)
+static unsigned int keyids[] = {
+	0xF5C75256,
+	0xe418e3aa,
+	0x897da07a,
+	0xdb42a60e,
+	0x4f2a6fd2,
+	0xcba29bf9,
+	0x2039b291,
+	0x30c9ecf8,
+	0x8df56d05,
+	0x1cddbca9,
+	0
+};
+
+static int readKeys(const char * uri)
 {
-    FD_t fd;
+    unsigned int * kip;
+    const byte * pkt;
+    ssize_t pktlen;
+    char fn[BUFSIZ];
+    pgpDig dig;
+    int rc;
+    int ec = 0;
 
-fprintf(stderr, "===== %s\n", path);
-    fd = Fopen(path, "r.ufdio");
-    if (fd != NULL) {
-	char buf[16*BUFSIZ];
-	size_t len = Fread(buf, 1, sizeof(buf), fd);
-	int xx = Fclose(fd);
+    for (kip = keyids; *kip; kip++) {
+	pgpArmor pa;
 
-	if (len > 0)
-	    fwrite(buf, 1, len, stderr);
+	sprintf(fn, "%s/pks/lookup?op=get&search=0x%x", uri, *kip);
+fprintf(stderr, "======================= %s\n", fn);
+	pkt = NULL;
+	pktlen = 0;
+	pa = pgpReadPkts(fn, &pkt, &pktlen);
+	if (pa == PGPARMOR_ERROR || pa == PGPARMOR_NONE
+         || pkt == NULL || pktlen <= 0)
+        {
+            ec++;
+            continue;
+        }
+
+	dig = pgpNewDig();
+	rc = pgpPrtPkts(pkt, pktlen, dig, _printing);
+	if (rc)
+	    ec++;
+	dig = pgpFreeDig(dig);
+
+	free((void *)pkt);
+	pkt = NULL;
     }
+
+    return ec;
 }
 
 static struct poptOption optionsTable[] = {
+ { "print", 'p', POPT_ARG_VAL,  &_printing, 1,		NULL, NULL },
+ { "noprint", 'n', POPT_ARG_VAL, &_printing, 0,		NULL, NULL },
  { "debug", 'd', POPT_ARG_VAL,	&_debug, -1,		NULL, NULL },
+ { "davdebug", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &_dav_debug, -1,
+	N_("debug protocol data stream"), NULL},
  { "ftpdebug", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &_ftp_debug, -1,
 	N_("debug protocol data stream"), NULL},
  { "noneon", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &noNeon, 1,
@@ -74,22 +111,7 @@ main(int argc, const char *argv[])
 	rpmIncreaseVerbosity();
     }
 
-_av_debug = -1;
-_ftp_debug = -1;
-_dav_debug = 1;
-#if 0
-    readKey(dirpath);
-#endif
-#if 0
-    readKey(ftppath);
-#endif
-    readKey(hkppath);
-#if 0
-    readKey(httppath);
-#endif
-#if 0
-    readKey(httpspath);
-#endif
+    readKeys(hkppath);
 
 /*@i@*/ urlFreeCache();
 
