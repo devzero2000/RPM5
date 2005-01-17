8eba2fb31588d103cb1f131f8098a5d7aa42f440
jbj <jbj>
2005-01-17 18:41:28 +0000

    Insure debugedit is built on linux.

--- scripts/macros.php
+++
@@ -1,9 +0,0 @@
-# Perl specific macro definitions.
-# To make use of these macros insert the following line into your spec file:
-# %include /opt/local/lib/rpm/macros.php
-
-%define		__find_requires	/opt/local/lib/rpm/find-php-requires
-%define		__find_provides	/opt/local/lib/rpm/find-php-provides
-
-%define		php_pear_dir	%{_datadir}/pear
-
