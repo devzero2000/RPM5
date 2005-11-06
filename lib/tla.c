3b61033c3d9102faf737ce4862de008c361de19d
jbj <jbj>
2005-11-06 09:40:13 +0000

    Closure checks for libtool(...) and pkgconfig(...) name spaces.

---
+++ lib/tla.c
@@ -0,0 +1,40 @@
+#include "system.h"
+
+#include <rpmlib.h>
+
+#include <rpmds.h>
+
+#include "debug.h"
+
+extern int _rpmds_debug;
+
+#define _LIBTOOL_PROVIDES  "/usr/bin/find /usr/lib -name '*.la' | /usr/lib/rpm/libtool.prov.sh"
+static const char * _libtool_provides = _LIBTOOL_PROVIDES;
+
+#define _LIBTOOL_REQUIRES  "/bin/rpm -qal | grep '\.la$' | /usr/lib/rpm/libtool.req.sh"
+static const char * _libtool_requires = _LIBTOOL_REQUIRES;
+
+int main(int argc, char *argv[])
+{
+    rpmds P = NULL;
+    rpmds R = NULL;
+    int rc;
+
+fprintf(stderr, "\n*** Gathering libtool Provides: using\n\t%s\n", _libtool_provides);
+    rc = rpmdsPipe(&P, RPMTAG_PROVIDENAME, _libtool_provides);
+fprintf(stderr, "\n*** Gathering libtool Requires: using\n\t%s\n", _libtool_requires);
+    rc = rpmdsPipe(&R, RPMTAG_REQUIRENAME, _libtool_requires);
+
+fprintf(stderr, "\n*** Checking libtool Requires: against Provides: closure --\n");
+    R = rpmdsInit(R);
+    while (rpmdsNext(R) >= 0) {
+	rc = rpmdsSearch(P, R);
+	if (rc < 0)
+	    fprintf(stderr, "%d %s\n", rpmdsIx(R), rpmdsDNEVR(R)+2);
+    }
+
+    P = rpmdsFree(P);
+    R = rpmdsFree(R);
+
+    return 0;
+}
---
+++ lib/tpc.c
@@ -0,0 +1,40 @@
+#include "system.h"
+
+#include <rpmlib.h>
+
+#include <rpmds.h>
+
+#include "debug.h"
+
+extern int _rpmds_debug;
+
+#define _PKGCONFIG_PROVIDES  "/usr/bin/find /usr/lib -name '*.pc' | /usr/lib/rpm/pkgconfig.prov.sh"
+static const char * _pkgconfig_provides = _PKGCONFIG_PROVIDES;
+
+#define _PKGCONFIG_REQUIRES  "/bin/rpm -qal | grep '\.pc$' | /usr/lib/rpm/pkgconfig.req.sh"
+static const char * _pkgconfig_requires = _PKGCONFIG_REQUIRES;
+
+int main(int argc, char *argv[])
+{
+    rpmds P = NULL;
+    rpmds R = NULL;
+    int rc;
+
+fprintf(stderr, "\n*** Gathering pkgconfig Provides: using\n\t%s\n", _pkgconfig_provides);
+    rc = rpmdsPipe(&P, RPMTAG_PROVIDENAME, _pkgconfig_provides);
+fprintf(stderr, "\n*** Gathering pkgconfig Requires: using\n\t%s\n", _pkgconfig_requires);
+    rc = rpmdsPipe(&R, RPMTAG_REQUIRENAME, _pkgconfig_requires);
+
+fprintf(stderr, "\n*** Checking pkgconfig Requires: against Provides: closure --\n");
+    R = rpmdsInit(R);
+    while (rpmdsNext(R) >= 0) {
+	rc = rpmdsSearch(P, R);
+	if (rc < 0)
+	    fprintf(stderr, "%d %s\n", rpmdsIx(R), rpmdsDNEVR(R)+2);
+    }
+
+    P = rpmdsFree(P);
+    R = rpmdsFree(R);
+
+    return 0;
+}
