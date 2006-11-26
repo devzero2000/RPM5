4fdbc29a32d502682f5f6a197076761b47cfe48c
jbj <jbj>
2006-11-26 17:40:50 +0000

    - make peace with autoconf-2.60 and automake-1.10.

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,13 @@ export CFLAGS
 export LDFLAGS
 
 LTV="libtoolize (GNU libtool) 1.5.22"
-ACV="autoconf (GNU Autoconf) 2.59"
-AMV="automake (GNU automake) 1.9.6"
+ACV="autoconf (GNU Autoconf) 2.61"
+AMV="automake (GNU automake) 1.10"
 USAGE="
 This script documents the versions of the tools I'm using to build rpm:
 	libtool-1.5.22
-	autoconf-2.59
-	automake-1.9.6
+	autoconf-2.61
+	automake-1.10
 Simply edit this script to change the libtool/autoconf/automake versions
 checked if you need to, as rpm should build (and has built) with all
 recent versions of libtool/autoconf/automake.
