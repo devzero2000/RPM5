98bc36b9ee4e2665815b0910a83c76e1ed826522
jbj <jbj>
2007-05-25 19:09:18 +0000

    Add new content from rpm-4_5 tip.

---
+++ rpmio/getpass.c
@@ -0,0 +1,41 @@
+/** \ingroup rpmio
+ * \file rpmio/getpass.c
+ */
+
+#include "system.h"
+#if HAVE_KEYUTILS_H
+#include <argv.h>
+#include <keyutils.h>
+#endif
+#include "debug.h"
+
+char * _GetPass(const char * prompt)
+{
+/*@-unrecog@*/
+    return getpass( prompt ? prompt : "" );
+/*@=unrecog@*/
+}
+
+char * _RequestPass(const char * prompt)
+{
+    static char * password = NULL;
+#if HAVE_KEYUTILS_H
+    const char * foo = "user rpm:yyyy spoon";
+    ARGV_t av = NULL;
+    int xx = argvSplit(&av, foo, NULL);
+    key_serial_t dest = 0;
+    key_serial_t key = 0;
+
+    if (password) {
+	free(password);
+	password = NULL;
+    }
+    key = request_key(av[0], av[1], av[2], dest);
+
+    xx = keyctl_read_alloc(key, (void *)&password);
+#endif
+
+    return password;
+}
+
+char * (*Getpass) (const char * prompt) = _GetPass;
