659d43ba8c9fb467f4ccd659669c79ecc2bd7e58
jbj <jbj>
2007-08-01 13:24:00 +0000

    start wrapping magic handling.

--- rpmio/tmagic.c
+++ rpmio/tmagic.c
@@ -14,28 +14,63 @@ static int _debug = 0;
 #define	FNPATH		"tmagic"
 static char * fnpath = FNPATH;
 
-static const char * Fmagic(const char * buffer, size_t length)
+typedef struct rpmmp_t * rpmmp;
+
+struct rpmmp_t {
+    const char * fn;
+    int flags;
+    magic_t ms;
+};
+
+static
+rpmmp rpmmpFree(rpmmp mp)
 {
-    int msflags = MAGIC_CHECK;
-    const char * magicfile = NULL;
-    magic_t ms = magic_open(msflags);
+    if (mp) {
+	if (mp->ms) {
+	    magic_close(mp->ms);
+	    mp->ms = NULL;
+	}
+	mp->fn = _free(mp->fn);
+	mp = _free(mp);
+    }
+    return NULL;
+}
+
+static
+rpmmp rpmmpNew(const char * fn, int flags)
+{
+    rpmmp mp = xcalloc(1, sizeof(*mp));
+    int xx;
+
+    if (fn)
+	mp->fn = xstrdup(fn);
+    mp->flags = (flags ? flags : MAGIC_CHECK);
+    mp->ms = magic_open(mp->ms);
+    if (mp->ms == NULL)
+	return rpmmpFree(mp);
+    xx = magic_load(mp->ms, mp->fn);
+    return mp;
+}
+
+static
+const char * rpmmpBuffer(rpmmp mp, const char * b, size_t nb)
+{
     const char * t = NULL;
 
-    if (ms) {
-	(void) magic_load(ms, magicfile);
-	t = magic_buffer(ms, buffer, length);
+    if (mp->ms) {
+	t = magic_buffer(mp->ms, b, nb);
 	t = xstrdup((t ? t : ""));
-	magic_close(ms);
     }
 
     return t;
 }
 
-static void readFile(const char * path)
+static
+void readFile(rpmmp mp, const char * path)
 {
     FD_t fd;
 
-fprintf(stderr, "===== %s\n", path);
+fprintf(stderr, "===== %s(%p, %s)\n", __FUNCTION__, mp, path);
     fd = Fopen(path, "r");
     if (fd != NULL) {
 	char buf[BUFSIZ];
@@ -43,8 +78,9 @@ fprintf(stderr, "===== %s\n", path);
 	(void) Fclose(fd);
 
 	if (len > 0) {
-	    const char * magic = Fmagic(buf, len);
+	    const char * magic = rpmmpBuffer(mp, buf, len);
 	    fprintf(stderr, "==> magic \"%s\"\n", magic);
+	    magic = _free(magic);
 	}
     }
 }
@@ -66,6 +102,7 @@ int
 main(int argc, const char *argv[])
 {
     poptContext optCon = poptGetContext(argv[0], argc, argv, optionsTable, 0);
+    rpmmp mp;
     int rc;
 
     while ((rc = poptGetNextOpt(optCon)) > 0) {
@@ -84,7 +121,9 @@ main(int argc, const char *argv[])
 	rpmIncreaseVerbosity();
     }
 
-    readFile(fnpath);
+    mp = rpmmpNew(NULL, 0);
+    readFile(mp, fnpath);
+    mp = rpmmpFree(mp);
 
 /*@i@*/ urlFreeCache();
 
