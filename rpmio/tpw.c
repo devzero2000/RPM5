eb3fea1de348ce3132d20a280fb1d4ea943de59a
jbj <jbj>
2007-08-22 19:09:15 +0000

    add tpw.c program to exercise _Requestkey. Add --with-keyutils=external.

---
+++ rpmio/tpw.c
@@ -0,0 +1,39 @@
+/**
+ * \file rpmio/rpmkey.c
+ */
+
+#include "system.h"
+#include <rpmio.h>
+#include <rpmcli.h>
+#include "debug.h"
+
+static int _debug = 0;
+
+static struct poptOption optionsTable[] = {
+ { "debug", 'd', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN,		&_debug, 1,
+	NULL, NULL },
+
+ { NULL, '\0', POPT_ARG_INCLUDE_TABLE, rpmcliAllPoptTable, 0,
+	N_("Common options for all rpm modes and executables:"),
+	NULL },
+
+    POPT_AUTOALIAS
+    POPT_AUTOHELP
+    POPT_TABLEEND
+};
+
+int
+main(int argc, char *const argv[])
+{
+    poptContext optCon = rpmcliInit(argc, argv, optionsTable);
+    const char * prompt = "Enter pass phrase: ";
+    int ret = 0;
+
+    if (optCon == NULL)
+        exit(EXIT_FAILURE);
+
+    fprintf(stderr, "password %s\n", _RequestPass(prompt));
+
+    optCon = rpmcliFini(optCon);
+    return ret;
+}
