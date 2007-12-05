f3b161601aee12207da115778f8b910d6bafba54
jbj <jbj>
2007-12-05 11:57:18 +0000

    - add placeholder for Class: and tags for %track vcheck(1) section.

--- rpmdb/rpmtag.h
+++ rpmdb/rpmtag.h
@@ -404,6 +404,9 @@ enum rpmTag_e {
     RPMTAG_RPMLIBVERSION	= 1199, /* i */
     RPMTAG_RPMLIBTIMESTAMP	= 1200, /* i */
     RPMTAG_RPMLIBVENDOR		= 1201, /* i */
+    RPMTAG_CLASS		= 1202, /* s (OpenPKG Class: placeholder) */
+    RPMTAG_TRACK		= 1203, /* s (OpenPKG %track) */
+    RPMTAG_TRACKPROG		= 1204, /* s */
 
 /*@-enummemuse@*/
     RPMTAG_FIRSTFREE_TAG	/*!< internal */
