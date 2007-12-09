044ad24d49fc455da3ae6dfaad1a6863265f4306
pkarlsen <pkarlsen>
2007-12-09 17:55:09 +0000

    make "rpm -bb --quiet" quiet as should be, otherwise this option is simply ignored in rpmcliAllPoptTable) (from pixel)

--- rpmqv.c
+++ rpmqv.c
@@ -68,6 +68,8 @@ enum modes {
 #define	MODES_FOR_TEST		(MODES_BT | MODES_IE)
 #define	MODES_FOR_ROOT		(MODES_BT | MODES_IE | MODES_QV | MODES_DB | MODES_K)
 
+static int quiet;
+
 /* the structure describing the options we take and the defaults */
 /*@unchecked@*/
 static struct poptOption optionsTable[] = {
@@ -121,6 +123,8 @@ static struct poptOption optionsTable[] = {
 	N_("Install/Upgrade/Erase options:"),
 	NULL },
 #endif	/* IAM_RPMEIU */
+
+ { "quiet", '\0', 0, &quiet, 0,			NULL, NULL},
 
  { NULL, '\0', POPT_ARG_INCLUDE_TABLE, rpmcliAllPoptTable, 0,
 	N_("Common options for all rpm modes and executables:"),
@@ -471,6 +475,9 @@ int main(int argc, const char ** argv)
 	}
     }
 
+    if (quiet)
+	rpmSetVerbosity(RPMMESS_QUIET);
+
 #if defined(IAM_RPMBT) || defined(IAM_RPMK)
     if (0
 #if defined(IAM_RPMBT)
@@ -628,7 +635,7 @@ int main(int argc, const char ** argv)
     case MODE_BUILD:
     case MODE_TARBUILD:
     {	const char * pkg;
-        while (!rpmIsVerbose())
+        if (!quiet) while (!rpmIsVerbose())
 	    rpmIncreaseVerbosity();
        
 	switch (ba->buildChar) {
