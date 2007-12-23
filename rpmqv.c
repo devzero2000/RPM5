e012294872814d4aaa011126f47e84b4a06233ec
rse <rse>
2007-12-23 20:20:14 +0000

    do no auto-verbosity on -bf, too

--- rpmqv.c
+++ rpmqv.c
@@ -607,8 +607,8 @@ int main(int argc, const char ** argv)
     case MODE_BUILD:
     case MODE_TARBUILD:
     {	const char * pkg;
-#if defined(RPM_VENDOR_OPENPKG) /* no-auto-verbose-increase-for-track */
-	if (ba->buildChar != 't')
+#if defined(RPM_VENDOR_OPENPKG) /* no-auto-verbose-increase-for-track-and-fetch */
+	if (ba->buildChar != 't' && ba->buildChar != 'f')
 #endif
         while (!rpmIsVerbose())
 	    rpmIncreaseVerbosity();
