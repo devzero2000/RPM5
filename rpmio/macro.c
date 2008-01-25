128860f7ec91d3368cf3fefa458b9b34b93ce4fb
rse <rse>
2008-01-25 22:19:12 +0000

    Provide a convenient %{shrink:<body>} macro to shrink the body by removing all leading and trailing whitespaces and reducing intermediate whitespaces to a single space character.
    Sidenode: this is for instance especially very handy if one plays macro
    tricks where mixed multi-level native+Lua expansions are performed and
    one wants to avoid trouble when generating intermediate Lua strings
    "..." on the fly (which cannot spawn multiple lines). Well, perhaps
    better not ask me for more details... ;-)

--- rpmio/macro.c
+++ rpmio/macro.c
@@ -1211,6 +1211,26 @@ doFoo(MacroBuf mb, int negate, const char * f, size_t fn,
         char *cp;
         if ((cp = getenv(buf)) != NULL)
             b = cp;
+    } else if (STREQ("shrink", f, fn)) {
+        /* shrink body by removing all leading and trailing whitespaces and
+           reducing intermediate whitespaces to a single space character */
+        int i, j, k, was_space = 0;
+        for (i = 0, j = 0, k = strlen(buf); i < k; ) {
+            if (xisspace((int)(buf[i]))) {
+                was_space = 1;
+                i++;
+                continue;
+            }
+            else if (was_space) {
+                was_space = 0;
+                if (j > 0) /* remove leading blanks at all */
+                    buf[j++] = ' ';
+                /* fallthrough */
+            }
+            buf[j++] = buf[i++];
+        }
+        buf[j] = '\0';
+        b = buf;
     } else if (STREQ("suffix", f, fn)) {
 	if ((b = strrchr(buf, '.')) != NULL)
 	    b++;
@@ -1606,6 +1626,7 @@ expandMacro(MacroBuf mb)
 	    STREQ("dirname", f, fn) ||
 	    STREQ("realpath", f, fn) ||
 	    STREQ("getenv", f, fn) ||
+	    STREQ("shrink", f, fn) ||
 	    STREQ("suffix", f, fn) ||
 	    STREQ("expand", f, fn) ||
 	    STREQ("verbose", f, fn) ||
