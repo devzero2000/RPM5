35e4052e1585a776eb6fa5f17164972cdc005193
jbj <jbj>
2008-02-02 01:33:19 +0000

    - jbj: mire.c: add STANDALONE test exerciser.

--- rpmio/mire.c
+++ rpmio/mire.c
@@ -3,6 +3,7 @@
  */
 #include "system.h"
 
+#include <rpmio.h>	/* XXX _free */
 #include <rpmlog.h>
 #define	_MIRE_INTERNAL
 #include <mire.h>
@@ -13,18 +14,6 @@
 
 /*@unchecked@*/
 int _mire_debug = 0;
-
-/**
- * Wrapper to free(3), hides const compilation noise, permit NULL, return NULL.
- * @param p		memory to free
- * @retval		NULL always
- */
-/*@unused@*/ static inline /*@null@*/ void *
-_free(/*@only@*/ /*@null@*/ const void * p) /*@modifies p@*/
-{
-    if (p != NULL)	free((void *)p);
-    return NULL;
-}
 
 int mireClean(miRE mire)
 {
@@ -40,17 +29,22 @@ fprintf(stderr, "--> mireClean(%p)\n", mire);
 	mire->preg = _free(mire->preg);
 	/*@=voidabstract =usereleased @*/
     }
-    memset(mire, 0, sizeof(*mire));	/* XXX trash and burn */
+    mire->fnflags = 0;
+    mire->cflags = 0;
+    mire->eflags = 0;
+    mire->notmatch = 0;
     return 0;
 }
 
 miRE XmireUnlink(miRE mire, const char * msg, const char * fn, unsigned ln)
 {
     if (mire == NULL) return NULL;
+
 /*@-modfilesys@*/
 if (_mire_debug && msg != NULL)
 fprintf(stderr, "--> mire %p -- %d %s at %s:%u\n", mire, mire->nrefs, msg, fn, ln);
 /*@=modfilesys@*/
+
     mire->nrefs--;
     return NULL;
 }
@@ -174,3 +168,82 @@ fprintf(stderr, "--> mireRegcomp(%p, \"%s\") rc %d\n", mire, pattern, rc);
 /*@=modfilesys@*/
     return rc;
 }
+
+/* =============================================================== */
+
+#if defined(STANDALONE)
+
+#include <rpmcb.h>
+#include <argv.h>
+#include <popt.h>
+
+static int _debug = 0;
+
+static struct poptOption optionsTable[] = {
+ { "debug", 'd', POPT_ARG_VAL,	&_debug, -1,		NULL, NULL },
+  POPT_AUTOHELP
+  POPT_TABLEEND
+};
+
+int
+main(int argc, char *argv[])
+{
+    poptContext optCon = poptGetContext(argv[0], argc, argv, optionsTable, 0);
+    miRE mire = NULL;
+    ARGV_t av = NULL;
+    int ac = 0;
+    int rc;
+    int xx;
+    int i;
+
+    while ((rc = poptGetNextOpt(optCon)) > 0) {
+        const char * optArg = poptGetOptArg(optCon);
+        optArg = _free(optArg);
+	switch (rc) {
+	case 'v':
+	    rpmIncreaseVerbosity();
+	    /*@switchbreak@*/ break;
+	default:
+	    poptPrintUsage(optCon, stderr, 0);
+	    goto exit;
+            /*@switchbreak@*/ break;
+	}
+    }
+
+    if (_debug) {
+	rpmIncreaseVerbosity();
+	rpmIncreaseVerbosity();
+_mire_debug = 1;
+    }
+
+    av = poptGetArgs(optCon);
+    ac = argvCount(av);
+    if (ac != 1) {
+	poptPrintUsage(optCon, stderr, 0);
+	goto exit;
+    }
+
+    mire = mireNew(RPMMIRE_REGEX, 0);
+    if ((xx = mireRegcomp(mire, argv[1])) != 0)
+	goto exit;
+    
+    xx = argvFgets(&av, NULL);
+    ac = argvCount(av);
+
+    for (i = 0; i < ac; i++) {
+	xx = mireRegexec(mire, av[i]);
+	if (xx == 0)
+	    fprintf(stdout, "%s\n", av[i]);
+    }
+
+exit:
+    mire = mireFree(mire);
+
+    av = argvFree(av);
+
+    optCon = poptFreeContext(optCon);
+
+    return 0;
+}
+
+#endif	/* STANDALONE */
