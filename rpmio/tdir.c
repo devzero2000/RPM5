288cd87426b626516b3c9825522b3fa80d9e402e
jbj <jbj>
2008-02-07 23:50:08 +0000

    - jbj: rpmio: rescuscitate tdir.c.

--- rpmio/tdir.c
+++ rpmio/tdir.c
@@ -3,18 +3,12 @@
 #include <rpmio_internal.h>
 #include <rpmmacro.h>
 #include <rpmcb.h>
+#include <argv.h>
 #include <popt.h>
 
 #include "debug.h"
 
 static int _debug = 0;
-
-#define	HTTPPATH	"https://wellfleet.jbj.org/rawhide/test/"
-#define	FTPPATH		"ftp://wellfleet.jbj.org/pub/rawhide/packages/test/"
-#define	DIRPATH		"/var/ftp/pub/rawhide/packages/test"
-static char * httppath = HTTPPATH;
-static char * ftppath = FTPPATH;
-static char * dirpath = DIRPATH;
 
 static void printDir(const char * path)
 {
@@ -44,6 +38,11 @@ dp->d_name);
 
 static struct poptOption optionsTable[] = {
  { "debug", 'd', POPT_ARG_VAL,	&_debug, -1,		NULL, NULL },
+
+ { "avdebug", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &_av_debug, -1,
+	N_("debug protocol data stream"), NULL},
+ { "davdebug", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &_dav_debug, -1,
+	N_("debug protocol data stream"), NULL},
  { "ftpdebug", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &_ftp_debug, -1,
 	N_("debug protocol data stream"), NULL},
  { "rpmiodebug", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &_rpmio_debug, -1,
@@ -59,9 +58,14 @@ int
 main(int argc, char *argv[])
 {
     poptContext optCon = poptGetContext(argv[0], argc, argv, optionsTable, 0);
+    ARGV_t av = NULL;
+    int ac;
+    const char * dn;
+
     int rc;
 
     while ((rc = poptGetNextOpt(optCon)) > 0) {
+	const char * optArg = poptGetOptArg(optCon);
 	switch (rc) {
 	case 'v':
 	    rpmIncreaseVerbosity();
@@ -69,21 +73,35 @@ main(int argc, char *argv[])
 	default:
             /*@switchbreak@*/ break;
 	}
+	optArg = _free(optArg);
     }
 
     if (_debug) {
 	rpmIncreaseVerbosity();
 	rpmIncreaseVerbosity();
+_av_debug = -1;
+_dav_debug = -1;
+_ftp_debug = -1;
     }
 
-_av_debug = -1;
-_ftp_debug = -1;
-_dav_debug = -1;
-    printDir(dirpath);
-    printDir(ftppath);
-    printDir(httppath);
+    av = poptGetArgs(optCon);
+    ac = argvCount(av);
+    if (ac < 1) {
+	poptPrintUsage(optCon, stderr, 0);
+	goto exit;
+    }
+
+    while ((dn = *av++) != NULL) {
+	dn = rpmGetPath(dn, "/", NULL);
+	printDir(dn);
+	dn = _free(dn);
+    }
+
+exit:
 
 /*@i@*/ urlFreeCache();
+
+    optCon = poptFreeContext(optCon);
 
     return 0;
 }
