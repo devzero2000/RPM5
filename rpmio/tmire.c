ad570ee1d2566b569b9cb23d4f3b17038176c012
jbj <jbj>
2008-02-10 02:08:27 +0000

    - rpmio: use tmire.c, not -DSTANDALONE, so librpmio doesnt rebuild.

--- rpmio/tmire.c
+++ rpmio/tmire.c
@@ -5,7 +5,22 @@
 
 #include "debug.h"
 
+static rpmMireMode mireMode = RPMMIRE_REGEX;
+static int invert = 0;
+
 static struct poptOption optionsTable[] = {
+
+ { "strcmp", '\0', POPT_ARG_VAL,	&mireMode, RPMMIRE_STRCMP,
+	N_("use strcmp matching"), NULL},
+ { "regex", '\0', POPT_ARG_VAL,		&mireMode, RPMMIRE_REGEX,
+	N_("use regex matching"), NULL},
+ { "fnmatch", '\0', POPT_ARG_VAL,	&mireMode, RPMMIRE_GLOB,
+	N_("use fnmatch matching"), NULL},
+ { "pcre", '\0', POPT_ARG_VAL,		&mireMode, RPMMIRE_PCRE,
+	N_("use pcre matching"), NULL},
+
+ { "invert-match", 'v', POPT_ARG_VAL|POPT_ARGFLAG_XOR,	&invert, 1,
+	N_("select non-mtching lines"), NULL},
 
  { NULL, '\0', POPT_ARG_INCLUDE_TABLE, rpmioAllPoptTable, 0,
 	N_("Common options for all rpmio executables:"),
@@ -22,7 +37,7 @@ main(int argc, char *argv[])
     miRE mire = NULL;
     ARGV_t av = NULL;
     int ac = 0;
-    int rc;
+    int rc = 1;
     int xx;
     int i;
 
@@ -31,31 +46,34 @@ _mire_debug = 1;
     }
 
     av = poptGetArgs(optCon);
-    ac = argvCount(av);
-    if (ac != 1) {
-	poptPrintUsage(optCon, stderr, 0);
+    if ((ac = argvCount(av)) != 1)
 	goto exit;
-    }
 
-    mire = mireNew(RPMMIRE_REGEX, 0);
-    if ((xx = mireRegcomp(mire, argv[1])) != 0)
+    mire = mireNew(mireMode, 0);
+    if ((rc = mireRegcomp(mire, av[0])) != 0)
 	goto exit;
+    mire->notmatch ^= invert;
     
-    xx = argvFgets(&av, NULL);
-    ac = argvCount(av);
+    av = NULL;
+    if ((rc = argvFgets(&av, NULL)) != 0)
+	goto exit;
 
+    rc = 1;	/* assume nomatch failure. */
+    ac = argvCount(av);
+    if (av && *av)
     for (i = 0; i < ac; i++) {
 	xx = mireRegexec(mire, av[i]);
-	if (xx == 0)
+	if (xx == 0) {
 	    fprintf(stdout, "%s\n", av[i]);
+	    rc = 0;
+	}
     }
+    av = argvFree(av);
 
 exit:
     mire = mireFree(mire);
 
-    av = argvFree(av);
+    optCon = rpmioFini(optCon);
 
-    optCon = poptFreeContext(optCon);
-
-    return 0;
+    return rc;
 }
