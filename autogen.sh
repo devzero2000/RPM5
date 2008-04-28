b4f5cdee1ebd6242b9e5b8dd3b928a136bf01081
jbj <jbj>
2008-04-28 16:40:28 +0000

    - jbj: preliminary support for internal -lzma (non-functional atm). - jbj: add lzma checkout to devtool system target.

--- autogen.sh
+++ autogen.sh
@@ -38,6 +38,9 @@ libtoolize () {
 echo "===> zlib"
 ( cd zlib && sh ./autogen.sh --noconfigure "$@" )
 echo "<=== zlib"
+echo "===> lzma"
+( cd lzma && sh ./autogen.sh --noconfigure "$@" )
+echo "<=== lzma"
 echo "===> file"
 ( cd file && sh ./autogen.sh --noconfigure "$@" )
 echo "<=== file"
