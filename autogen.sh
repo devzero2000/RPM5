dd2838fb00195036793108de567fdb00a640a6c6
rse <rse>
2008-05-25 08:37:10 +0000

    be smart and allow one to still build RPM from CVS with old GNU libtool 1.5.x

--- autogen.sh
+++ autogen.sh
@@ -17,6 +17,7 @@ installed developer tools are mandatory:
 #   wrapper for running GNU libtool's libtoolize(1)
 libtoolize () {
     _libtoolize=`which glibtoolize 2>/dev/null`
+    _libtoolize_args="$*"
     case "$_libtoolize" in
         /* ) ;;
         *  ) _libtoolize=`which libtoolize 2>/dev/null`
@@ -26,7 +27,11 @@ libtoolize () {
              esac
              ;;
     esac
-    $_libtoolize ${1+"$@"}
+    _libtoolize_version="`$_libtoolize --version | sed -e '1q' | sed -e 's;^[^0-9];;'`"
+    case "$_libtoolize_version" in
+        1.* ) _libtoolize_args=`echo "X$_libtoolize_args" | sed -e 's;^X;;' -e 's;--quiet;;' -e 's;--install;;'` ;;
+    esac
+    eval $_libtoolize $_libtoolize_args
 }
 
 #   requirements sanity check
