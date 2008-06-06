d32162c6eb25174a69ffc2c0934e04e55373b0f9
jbj <jbj>
2008-06-06 17:36:23 +0000

    - tests: verify setup using --version/--querytags/--showrc. - display noisy getconf()/uname() info with -vv --showrc instead.

--- lib/rpmrc.c
+++ lib/rpmrc.c
@@ -1060,7 +1060,9 @@ int rpmShowRC(FILE * fp)
 	    ds = rpmdsFree(ds);
 	    fprintf(fp, "\n");
 	}
+    }
 
+    if (rpmIsDebug()) {
 	xx = rpmdsGetconf(&ds, NULL);
 	if (ds != NULL) {
 	    fprintf(fp,
