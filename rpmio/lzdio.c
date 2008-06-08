4fa3ec6459f9aee126af962430ff16b3bde3890b
jbj <jbj>
2008-06-08 20:29:16 +0000

    - jbj: lzdio: start using -llzma instead.

--- rpmio/lzdio.c
+++ rpmio/lzdio.c
@@ -6,62 +6,177 @@
 #include <rpmmacro.h>
 #include <rpmcb.h>
 
-#include "LzmaDecode.h"
+#include <lzma.h>
 
 #include "debug.h"
 
-#include "LzmaDecode.c"
-
-#define kInBufferSize (1 << 15)
-
-typedef struct _CBuffer {
-  ILzmaInCallback InCallback;
-/*@dependent@*/
-  FILE *File;
-  unsigned char Buffer[kInBufferSize];
-} CBuffer;
-
-typedef struct lzfile {
-    CBuffer g_InBuffer;
-    CLzmaDecoderState state;  /* it's about 24-80 bytes structure, if int is 32-bit */
-    unsigned char properties[LZMA_PROPERTIES_SIZE];
-
-#if 0
-    FILE *file;
-#endif
-    pid_t pid;
-} LZFILE;
-
 #define	LZDONLY(fd)	assert(fdGetIo(fd) == lzdio)
 
-/*@access FD_t @*/
+#define kBufferSize (1 << 15)
+
+typedef struct lzfile {
+  /* IO buffer */
+    uint8_t buf[kBufferSize];
+
+    lzma_stream strm;
+
+    FILE *fp;
+
+    int encoding;
+    int eof;
+
+} LZFILE;
+
+static LZFILE *lzopen_internal(const char *path, const char *mode, int fd)
+{
+    int level = 5;
+    int encoding = 0;
+    FILE *fp;
+    LZFILE *lzfile;
+    lzma_ret ret;
+
+    for (; *mode; mode++) {
+	if (*mode == 'w')
+	    encoding = 1;
+	else if (*mode == 'r')
+	    encoding = 0;
+	else if (*mode >= '1' && *mode <= '9')
+	    level = *mode - '0';
+    }
+    if (fd != -1)
+	fp = fdopen(fd, encoding ? "w" : "r");
+    else
+	fp = fopen(path, encoding ? "w" : "r");
+    if (!fp)
+	return 0;
+    lzfile = calloc(1, sizeof(*lzfile));
+    if (!lzfile) {
+	fclose(fp);
+	return 0;
+    }
+    lzfile->fp = fp;
+    lzfile->encoding = encoding;
+    lzfile->eof = 0;
+    lzfile->strm = LZMA_STREAM_INIT_VAR;
+    if (encoding) {
+	lzma_options_alone alone;
+	alone.uncompressed_size = LZMA_VLI_VALUE_UNKNOWN;
+	memcpy(&alone.lzma, &lzma_preset_lzma[level - 1], sizeof(alone.lzma));
+	ret = lzma_alone_encoder(&lzfile->strm, &alone);
+    } else {
+	ret = lzma_auto_decoder(&lzfile->strm, 0, 0);
+    }
+    if (ret != LZMA_OK) {
+	fclose(fp);
+	free(lzfile);
+	return 0;
+    }
+    return lzfile;
+}
+
+static LZFILE *lzopen(const char *path, const char *mode)
+{
+    return lzopen_internal(path, mode, -1);
+}
+
+static LZFILE *lzdopen(int fd, const char *mode)
+{
+    if (fd < 0)
+	return 0;
+    return lzopen_internal(0, mode, fd);
+}
+
+#ifdef	UNUSED
+static int lzflush(LZFILE *lzfile)
+{
+    return fflush(lzfile->fp);
+}
+#endif
+
+static int lzclose(LZFILE *lzfile)
+{
+    lzma_ret ret;
+    size_t n;
+
+    if (!lzfile)
+	return -1;
+    if (lzfile->encoding) {
+	for (;;) {
+	    lzfile->strm.avail_out = kBufferSize;
+	    lzfile->strm.next_out = lzfile->buf;
+	    ret = lzma_code(&lzfile->strm, LZMA_FINISH);
+	    if (ret != LZMA_OK && ret != LZMA_STREAM_END)
+		return -1;
+	    n = kBufferSize - lzfile->strm.avail_out;
+	    if (n && fwrite(lzfile->buf, 1, n, lzfile->fp) != n)
+		return -1;
+	    if (ret == LZMA_STREAM_END)
+		break;
+	}
+    }
+    lzma_end(&lzfile->strm);
+    return fclose(lzfile->fp);
+    free(lzfile);
+}
+
+static ssize_t lzread(LZFILE *lzfile, void *buf, size_t len)
+{
+    lzma_ret ret;
+    int eof = 0;
+
+    if (!lzfile || lzfile->encoding)
+      return -1;
+    if (lzfile->eof)
+      return 0;
+    lzfile->strm.next_out = buf;
+    lzfile->strm.avail_out = len;
+    for (;;) {
+	if (!lzfile->strm.avail_in) {
+	    lzfile->strm.next_in = lzfile->buf;
+	    lzfile->strm.avail_in = fread(lzfile->buf, 1, kBufferSize, lzfile->fp);
+	    if (!lzfile->strm.avail_in)
+		eof = 1;
+	}
+	ret = lzma_code(&lzfile->strm, LZMA_RUN);
+	if (ret == LZMA_STREAM_END) {
+	    lzfile->eof = 1;
+	    return len - lzfile->strm.avail_out;
+	}
+	if (ret != LZMA_OK)
+	    return -1;
+	if (!lzfile->strm.avail_out)
+	    return len;
+	if (eof)
+	    return -1;
+      }
+}
+
+static ssize_t lzwrite(LZFILE *lzfile, void *buf, size_t len)
+{
+    lzma_ret ret;
+    size_t n;
+
+    if (!lzfile || !lzfile->encoding)
+	return -1;
+    if (!len)
+	return 0;
+    lzfile->strm.next_in = buf;
+    lzfile->strm.avail_in = len;
+    for (;;) {
+	lzfile->strm.next_out = lzfile->buf;
+	lzfile->strm.avail_out = kBufferSize;
+	ret = lzma_code(&lzfile->strm, LZMA_RUN);
+	if (ret != LZMA_OK)
+	    return -1;
+	n = kBufferSize - lzfile->strm.avail_out;
+	if (n && fwrite(lzfile->buf, 1, n, lzfile->fp) != n)
+	    return -1;
+	if (!lzfile->strm.avail_in)
+	    return len;
+    }
+}
 
 /* =============================================================== */
-
-static size_t MyReadFile(FILE *file, void *data, size_t size)
-	/*@globals fileSystem @*/
-	/*@modifies *file, *data, fileSystem @*/
-{ 
-    if (size == 0) return 0;
-    return fread(data, 1, size, file); 
-}
-
-static int MyReadFileAndCheck(FILE *file, void *data, size_t size)
-	/*@globals fileSystem @*/
-	/*@modifies *file, *data, fileSystem @*/
-{
-    return (MyReadFile(file, data, size) == size);
-}
-
-static int LzmaReadCompressed(void *object, const unsigned char **buffer, SizeT *size)
-	/*@globals fileSystem @*/
-	/*@modifies *buffer, *size, fileSystem @*/
-{
-    CBuffer *b = object;
-    *buffer = b->Buffer;
-    *size = MyReadFile(b->File, b->Buffer, kInBufferSize);
-    return LZMA_RESULT_OK;
-}
 
 static inline /*@dependent@*/ void * lzdFileno(FD_t fd)
 	/*@*/
@@ -71,144 +186,32 @@ static inline /*@dependent@*/ void * lzdFileno(FD_t fd)
 
     FDSANE(fd);
     for (i = fd->nfps; i >= 0; i--) {
-	FDSTACK_t * fps = &fd->fps[i];
-	if (fps->io != lzdio)
-	    continue;
-	rc = fps->fp;
-    	break;
+/*@-boundsread@*/
+	    FDSTACK_t * fps = &fd->fps[i];
+/*@=boundsread@*/
+	    if (fps->io != lzdio)
+		continue;
+	    rc = fps->fp;
+	break;
     }
     
     return rc;
 }
 
-/*@-mods@*/	/* XXX hide rpmGlobalMacroContext mods for now. */
-static LZFILE * lzdWriteOpen(int fdno, const char * fmode)
-	/*@globals fileSystem, internalState @*/
-	/*@modifies fileSystem, internalState @*/
-{
-    pid_t pid;
-    int p[2];
-    int xx;
-    const char *lzma;
-    char l[3] = "-7";	/* XXX same as default */
-    const char *level;
-
-    /* revisit use of LZMA_Alone, when lzdRead supports new LZMA Utils */
-    char *env[] = { "LZMA_OPT=--format=alone", NULL };
-
-    if (fmode != NULL && fmode[0] == 'w' && xisdigit(fmode[1])) /* "w9" */
-	l[1] = fmode[1];
-    level = l;
-
-    if (fdno < 0) return NULL;
-    if (pipe(p) < 0) {
-	xx = close(fdno);
-	return NULL;
-    }
-    pid = fork();
-    if (pid < 0) {
-	xx = close(fdno);
-	return NULL;
-    }
-    if (pid) {
-	LZFILE * lzfile = xcalloc(1, sizeof(*lzfile));
-
-	xx = close(fdno);
-	xx = close(p[0]);
-	lzfile->pid = pid;
-	lzfile->g_InBuffer.File = fdopen(p[1], "wb");
-	if (lzfile->g_InBuffer.File == NULL) {
-	    xx = close(p[1]);
-	    lzfile = _free(lzfile);
-	    return NULL;
-	}
-	return lzfile;
-    } else {
-	int i;
-	/* lzma */
-	xx = close(p[1]);
-	xx = dup2(p[0], 0);
-	xx = dup2(fdno, 1);
-	for (i = 3; i < 1024; i++)
-	    xx = close(i);
-	lzma = rpmGetPath("%{?__lzma}%{!?__lzma:/usr/bin/lzma}", NULL);
-	if (execle(lzma, "lzma", level, NULL, env))
-	    _exit(1);
-	lzma = _free(lzma);
-    }
-    return NULL; /* warning */
-}
-/*@=mods@*/
-
-static LZFILE * lzdReadOpen(int fdno)
+/*@-globuse@*/
+static /*@null@*/ FD_t lzdOpen(const char * path, const char * fmode)
 	/*@globals fileSystem @*/
 	/*@modifies fileSystem @*/
 {
-    LZFILE * lzfile;
-    unsigned char ff[8];
-    size_t nb;
-
-    if (fdno < 0) return NULL;
-    lzfile = xcalloc(1, sizeof(*lzfile));
-    if (lzfile == NULL) return NULL;
-    lzfile->g_InBuffer.File = fdopen(fdno, "rb");
-    if (lzfile->g_InBuffer.File == NULL) goto error2;
-
-    if (!MyReadFileAndCheck(lzfile->g_InBuffer.File, lzfile->properties, sizeof(lzfile->properties)))
-	goto error;
-
-    memset(ff, 0, sizeof(ff));
-    if (!MyReadFileAndCheck(lzfile->g_InBuffer.File, ff, 8)) goto error;
-    if (LzmaDecodeProperties(&lzfile->state.Properties, lzfile->properties, LZMA_PROPERTIES_SIZE) != LZMA_RESULT_OK)
-	goto error;
-    nb = LzmaGetNumProbs(&lzfile->state.Properties) * sizeof(*lzfile->state.Probs);
-    lzfile->state.Probs = xmalloc(nb);
-    if (lzfile->state.Probs == NULL) goto error;
-
-    if (lzfile->state.Properties.DictionarySize == 0)
-	lzfile->state.Dictionary = 0;
-    else {
-	lzfile->state.Dictionary = xmalloc(lzfile->state.Properties.DictionarySize);
-	if (lzfile->state.Dictionary == NULL) {
-	    lzfile->state.Probs = _free(lzfile->state.Probs);
-	    goto error;
-	}
-    }
-    lzfile->g_InBuffer.InCallback.Read = LzmaReadCompressed;
-    LzmaDecoderInit(&lzfile->state);
-
-    return lzfile;
-
-error:
-    (void) fclose(lzfile->g_InBuffer.File);
-error2:
-    lzfile = _free(lzfile);
-    return NULL;
-}
-
-/*@-globuse@*/
-static /*@null@*/ FD_t lzdOpen(const char * path, const char * fmode)
-	/*@globals fileSystem, internalState @*/
-	/*@modifies fileSystem, internalState @*/
-{
     FD_t fd;
     mode_t mode = (fmode && fmode[0] == 'w' ? O_WRONLY : O_RDONLY);
-    int fdno = open(path, mode);
-    LZFILE * lzfile = (mode == O_WRONLY)
-	? lzdWriteOpen(fdno, fmode)
-	: lzdReadOpen(fdno);
+    LZFILE * lzfile = lzopen(path, fmode);
 
-    if (lzfile == NULL) {
-	if (fdno >= 0) (void) close(fdno);
+    if (lzfile == NULL)
 	return NULL;
-    }
-
     fd = fdNew("open (lzdOpen)");
     fdPop(fd); fdPush(fd, lzdio, lzfile, -1);
-    fdSetOpen(fd, path, fdno, mode);
-
-DBGIO(fd, (stderr, "==>\tlzdOpen(\"%s\", \"%s\") fd %p %s\n", path, fmode, (fd ? fd : NULL), fdbg(fd)));
-
+    fdSetOpen(fd, path, fileno(lzfile->fp), mode);
     return fdLink(fd, "lzdOpen");
 }
 /*@=globuse@*/
@@ -220,13 +223,12 @@ static /*@null@*/ FD_t lzdFdopen(void * cookie, const char * fmode)
 {
     FD_t fd = c2f(cookie);
     int fdno = fdFileno(fd);
-    LZFILE * lzfile;
+    LZFILE *lzfile;
 
 assert(fmode != NULL);
-    fdSetFdno(fd, -1);		/* XXX skip the fdio close */
-    lzfile = (fmode[0] == 'w')
-	? lzdWriteOpen(fdno, fmode)
-	: lzdReadOpen(fdno);
+    fdSetFdno(fd, -1);          /* XXX skip the fdio close */
+    if (fdno < 0) return NULL;
+    lzfile = lzdopen(fdno, fmode);
     if (lzfile == NULL) return NULL;
     fdPush(fd, lzdio, lzfile, fdno);
     return fdLink(fd, "lzdFdopen");
@@ -239,40 +241,32 @@ static int lzdFlush(FD_t fd)
 	/*@globals fileSystem @*/
 	/*@modifies fileSystem @*/
 {
-    LZFILE *lzfile = lzdFileno(fd);
-
-    if (lzfile == NULL || lzfile->g_InBuffer.File == NULL) return -2;
-    return fflush(lzfile->g_InBuffer.File);
+    return lzflush(lzdFileno(fd));
 }
 /*@=globuse@*/
 #endif
 
 /* =============================================================== */
 /*@-globuse@*/
-/*@-mustmod@*/		/* LCL: *buf is modified */
+/*@-mustmod@*/          /* LCL: *buf is modified */
 static ssize_t lzdRead(void * cookie, /*@out@*/ char * buf, size_t count)
 	/*@globals fileSystem, internalState @*/
-	/*@modifies buf, fileSystem, internalState @*/
+	/*@modifies *buf, fileSystem, internalState @*/
 {
     FD_t fd = c2f(cookie);
     LZFILE *lzfile;
     ssize_t rc = -1;
-    size_t out;
-    int res = 0;
 
-    if (fd->bytesRemain == 0) return 0;	/* XXX simulate EOF */
+    if (fd->bytesRemain == 0) return 0; /* XXX simulate EOF */
     lzfile = lzdFileno(fd);
+assert(lzfile != NULL);
     fdstat_enter(fd, FDSTAT_READ);
-    if (lzfile->g_InBuffer.File) {
 /*@-compdef@*/
-	res = LzmaDecode(&lzfile->state, &lzfile->g_InBuffer.InCallback, (unsigned char *)buf, count, &out);
-	rc = (ssize_t)out;
-    }
+    rc = lzread(lzfile, buf, count);
 /*@=compdef@*/
 DBGIO(fd, (stderr, "==>\tlzdRead(%p,%p,%u) rc %lx %s\n", cookie, buf, (unsigned)count, (unsigned long)rc, fdbg(fd)));
-    if (res) {
-	if (lzfile)
-	    fd->errcookie = "Lzma: decoding error";
+    if (rc == -1) {
+	fd->errcookie = "Lzma: decoding error";
     } else if (rc >= 0) {
 	fdstat_exit(fd, FDSTAT_READ, rc);
 	/*@-compdef@*/
@@ -291,24 +285,24 @@ static ssize_t lzdWrite(void * cookie, const char * buf, size_t count)
 {
     FD_t fd = c2f(cookie);
     LZFILE *lzfile;
-    ssize_t rc;
+    ssize_t rc = 0;
 
-    if (fd->bytesRemain == 0) return 0;	/* XXX simulate EOF */
+    if (fd == NULL || fd->bytesRemain == 0) return 0;   /* XXX simulate EOF */
 
     if (fd->ndigests && count > 0) fdUpdateDigests(fd, (void *)buf, count);
 
     lzfile = lzdFileno(fd);
+
     fdstat_enter(fd, FDSTAT_WRITE);
-    rc = fwrite((void *)buf, 1, count, lzfile->g_InBuffer.File);
+    rc = lzwrite(lzfile, (void *)buf, count);
 DBGIO(fd, (stderr, "==>\tlzdWrite(%p,%p,%u) rc %lx %s\n", cookie, buf, (unsigned)count, (unsigned long)rc, fdbg(fd)));
-    if (rc == -1) {
-	fd->errcookie = strerror(ferror(lzfile->g_InBuffer.File));
+    if (rc < 0) {
+	fd->errcookie = "Lzma: encoding error";
     } else if (rc > 0) {
 	fdstat_exit(fd, FDSTAT_WRITE, rc);
     }
     return rc;
 }
-/*@=globuse@*/
 
 static inline int lzdSeek(void * cookie, /*@unused@*/ _libio_pos_t pos,
 			/*@unused@*/ int whence)
@@ -325,31 +319,22 @@ static int lzdClose( /*@only@*/ void * cookie)
 	/*@modifies fileSystem, internalState @*/
 {
     FD_t fd = c2f(cookie);
-    LZFILE * lzfile = lzdFileno(fd);
+    LZFILE *lzfile;
     int rc;
+
+    lzfile = lzdFileno(fd);
 
     if (lzfile == NULL) return -2;
     fdstat_enter(fd, FDSTAT_CLOSE);
-/*@-noeffectuncon@*/ /* FIX: check rc */
-    rc = fclose(lzfile->g_InBuffer.File);
-    if (lzfile->pid)
-	rc = (int) wait4(lzfile->pid, NULL, 0, NULL);
-    else { /* reading */
-	lzfile->state.Probs = _free(lzfile->state.Probs);
-	lzfile->state.Dictionary = _free(lzfile->state.Dictionary);
-    }
-/*@-dependenttrans@*/
-    lzfile = _free(lzfile);
-/*@=dependenttrans@*/
-/*@=noeffectuncon@*/
-    rc = 0;	/* XXX FIXME */
+    /*@-dependenttrans@*/
+    rc = lzclose(lzfile);
+    /*@=dependenttrans@*/
 
     /* XXX TODO: preserve fd if errors */
 
     if (fd) {
 	if (rc == -1) {
-assert(lzfile != NULL);	/* XXX FIXME, lzfile is always NULL here. */
-	    fd->errcookie = strerror(ferror(lzfile->g_InBuffer.File));
+	    fd->errcookie = strerror(ferror(lzfile->fp));
 	} else if (rc >= 0) {
 	    fdstat_exit(fd, FDSTAT_CLOSE, rc);
 	}
@@ -358,14 +343,16 @@ assert(lzfile != NULL);	/* XXX FIXME, lzfile is always NULL here. */
 DBGIO(fd, (stderr, "==>\tlzdClose(%p) rc %lx %s\n", cookie, (unsigned long)rc, fdbg(fd)));
 
     if (_rpmio_debug || rpmIsDebug()) fdstat_print(fd, "LZDIO", stderr);
+    /*@-branchstate@*/
     if (rc == 0)
 	fd = fdFree(fd, "open (lzdClose)");
+    /*@=branchstate@*/
     return rc;
 }
 
 /*@-type@*/ /* LCL: function typedefs */
 static struct FDIO_s lzdio_s = {
-  lzdRead, lzdWrite, lzdSeek, lzdClose,	lzdOpen, lzdFdopen,
+  lzdRead, lzdWrite, lzdSeek, lzdClose, lzdOpen, lzdFdopen,
 };
 /*@=type@*/
 
