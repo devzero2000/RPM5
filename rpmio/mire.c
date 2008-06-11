179aade14d194eceeca3cf1ec462dc06a9071eff
jbj <jbj>
2008-06-11 20:13:18 +0000

    - jbj: fix: reattach RPMMIRE_DEFAULT as posix regex, not strcmp, pattern.

--- rpmio/mire.c
+++ rpmio/mire.c
@@ -140,9 +140,9 @@ int mireRegcomp(miRE mire, const char * pattern)
     mire->pattern = xstrdup(pattern);
 
     switch (mire->mode) {
-    case RPMMIRE_DEFAULT:
     case RPMMIRE_STRCMP:
 	break;
+    case RPMMIRE_DEFAULT:
     case RPMMIRE_REGEX:
 	mire->preg = xcalloc(1, sizeof(*mire->preg));
 	if (mire->cflags == 0)
