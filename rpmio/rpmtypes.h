32640a54c85efbf8c41c00c39edc604c58e4a4bb
jbj <jbj>
2008-07-30 11:40:24 +0000

    - jbj: private typedefs for <stdint.h> excision.

---
+++ rpmio/rpmtypes.h
@@ -0,0 +1,13 @@
+#ifndef _H_RPMTYPES_
+#define	_H_RPMTYPES_
+
+/** \ingroup rpmio
+ * \file rpmio/rpmtypes.h
+ */
+
+typedef	unsigned char		rpmuint8_t;
+typedef unsigned short		rpmuint16_t;
+typedef unsigned int		rpmuint32_t;
+typedef unsigned long long	rpmuint64_t;
+
+#endif /* _H_ARGV_ */
