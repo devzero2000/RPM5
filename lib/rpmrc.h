d2bb41989117332e38788eae18847bdd69e9fb86
jbj <jbj>
2008-08-01 10:55:13 +0000

    - jbj: add rpmrc.h.

---
+++ lib/rpmrc.h
@@ -0,0 +1,90 @@
+#ifndef H_RPMRC
+#define	H_RPMRC
+
+/**
+ * \file lib/rpmrc.h
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** \ingroup rpmrc
+ * Build and install arch/os table identifiers.
+ * @deprecated Eliminate from API.
+ * @todo	Eliminate in rpm-5.1.
+ */
+enum rpm_machtable_e {
+    RPM_MACHTABLE_INSTARCH	= 0,	/*!< Install platform architecture. */
+    RPM_MACHTABLE_INSTOS	= 1,	/*!< Install platform operating system. */
+    RPM_MACHTABLE_BUILDARCH	= 2,	/*!< Build platform architecture. */
+    RPM_MACHTABLE_BUILDOS	= 3	/*!< Build platform operating system. */
+};
+#define	RPM_MACHTABLE_COUNT	4	/*!< No. of arch/os tables. */
+
+/** \ingroup rpmrc
+ * Read macro configuration file(s) for a target.
+ * @param file		NULL always
+ * @param target	target platform (NULL uses default)
+ * @return		0 on success, -1 on error
+ */
+int rpmReadConfigFiles(/*@null@*/ const char * file,
+		/*@null@*/ const char * target)
+	/*@globals rpmGlobalMacroContext, rpmCLIMacroContext, h_errno,
+		fileSystem, internalState @*/
+	/*@modifies rpmGlobalMacroContext, rpmCLIMacroContext,
+		fileSystem, internalState @*/;
+
+/*@only@*/ /*@null@*/ /*@unchecked@*/
+extern void * platpat;
+/*@unchecked@*/
+extern int nplatpat;
+
+/** \ingroup rpmrc
+ * Return score of a platform string.
+ * A platform score measures the "nearness" of a platform string wrto
+ * configured platform patterns. The returned score is the line number
+ * of the 1st pattern in /etc/rpm/platform that matches the input string.
+ *
+ * @param platform	cpu-vendor-os platform string
+ * @param mi_re		pattern array (NULL uses /etc/rpm/platform patterns)
+ * @param mi_nre	no. of patterns
+ * @return		platform score (0 is no match, lower is preferred)
+ */
+int rpmPlatformScore(const char * platform, /*@null@*/ void * mi_re, int mi_nre)
+	/*@modifies mi_re @*/;
+
+/** \ingroup rpmrc
+ * Display current rpmrc (and macro) configuration.
+ * @param fp		output file handle
+ * @return		0 always
+ */
+int rpmShowRC(FILE * fp)
+	/*@globals rpmGlobalMacroContext, h_errno, fileSystem, internalState @*/
+	/*@modifies *fp, rpmGlobalMacroContext, fileSystem, internalState  @*/;
+
+/** \ingroup rpmrc
+ * @deprecated Use addMacro to set _target_* macros.
+ * @todo	Eliminate in rpm-5.1.
+ # @note Only used by build code.
+ * @param archTable
+ * @param osTable
+ */
+void rpmSetTables(int archTable, int osTable)
+	/*@globals rpmGlobalMacroContext, h_errno, fileSystem, internalState @*/
+	/*@modifies rpmGlobalMacroContext, fileSystem, internalState @*/;
+
+/** \ingroup rpmrc
+ * @todo	Eliminate in rpm-5.1.
+ * Destroy rpmrc arch/os compatibility tables.
+ * @todo Eliminate from API.
+ */
+void rpmFreeRpmrc(void)
+	/*@globals platpat, nplatpat, internalState @*/
+	/*@modifies platpat, nplatpat, internalState @*/;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* H_RPMRC */
