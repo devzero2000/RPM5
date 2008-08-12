df37096d8f55a10c9495541d16d3acd7b51b91bd
jbj <jbj>
2008-08-12 18:46:12 +0000

    - jbj: permit building with older libtool-1.5.26 (back port from HEAD).

--- autogen.sh
+++ autogen.sh
@@ -8,15 +8,16 @@ GTT="gettextize (GNU gettext-tools) 0.17"
 USAGE="
 To build RPM from plain CVS sources the following
 installed developer tools are mandatory:
-    GNU automake  1.10
-    GNU autoconf  2.62
-    GNU libtool   2.2.4
-    GNU gettext   0.17
+    $AMV
+    $ACV
+    $LTV
+    $GTT
 "
 
 #   wrapper for running GNU libtool's libtoolize(1)
 libtoolize () {
     _libtoolize=`which glibtoolize 2>/dev/null`
+    _libtoolize_args="$*"
     case "$_libtoolize" in
         /* ) ;;
         *  ) _libtoolize=`which libtoolize 2>/dev/null`
@@ -26,7 +27,11 @@ libtoolize () {
              esac
              ;;
     esac
-    $_libtoolize ${1+"$@"}
+    _libtoolize_version="`$_libtoolize --version | sed -e '1q' | sed -e 's;^[^0-9]*;;'`"
+    case "$_libtoolize_version" in
+        1.* ) _libtoolize_args=`echo "X$_libtoolize_args" | sed -e 's;^X;;' -e 's;--quiet;;' -e 's;--install;;'` ;;
+    esac
+    eval $_libtoolize $_libtoolize_args
 }
 
 #   requirements sanity check
