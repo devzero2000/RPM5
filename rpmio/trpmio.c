cd1be2ee062a2bd05105259f0a516320e3ad123b
afb <afb>
2008-12-29 22:48:38 +0000

    use ufdio for test, avoids just getting ENOENT

--- rpmio/trpmio.c
+++ rpmio/trpmio.c
@@ -12,10 +12,10 @@ int main(int argc, char *argv[])
     FD_t f1, f2, f3, f4, f5;
  
     fprintf (stderr, "open http://www.gnome.org/\n");
-    f1 = Fopen ("http://www.gnome.org/", "r");
+    f1 = Fopen ("http://www.gnome.org/", "r.ufdio");
  
     fprintf (stderr, "open http://people.redhat.com/\n");
-    f2 = Fopen ("http://people.redhat.com/", "r");
+    f2 = Fopen ("http://people.redhat.com/", "r.ufdio");
  
   if (f1) {
     fprintf (stderr, "close http://www.gnome.org/\n");
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
   }
  
     fprintf (stderr, "open http://www.redhat.com/\n");
-    f3 = Fopen ("http://www.redhat.com/", "r");
+    f3 = Fopen ("http://www.redhat.com/", "r.ufdio");
  
   if (f2) {
     fprintf (stderr, "close http://people.redhat.com/\n");
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
   }
  
     fprintf (stderr, "open http://www.wraptastic.org/\n");
-    f4 = Fopen ("http://www.wraptastic.org/", "r");
+    f4 = Fopen ("http://www.wraptastic.org/", "r.ufdio");
  
   if (f3) {
     fprintf (stderr, "close http://people.redhat.com/\n");
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
   }
  
     fprintf (stderr, "open http://people.redhat.com/\n");
-    f5 = Fopen ("http://people.redhat.com/", "r");
+    f5 = Fopen ("http://people.redhat.com/", "r.ufdio");
  
   if (f4) {
     fprintf (stderr, "close http://www.wraptastic.org/\n");
