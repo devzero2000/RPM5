53f70f04daf4dc8d3c28dbfc21ec11ed754312e8
jbj <jbj>
2009-02-11 11:08:52 +0000

    - rpm.org: Handle broken pkgconfig files better, rhbz#484970 (Lubomir Rintel).

--- scripts/pkgconfigdeps.sh
+++ scripts/pkgconfigdeps.sh
@@ -23,7 +23,11 @@ case $1 in
 	$pkgconfig --print-provides "$filename" 2> /dev/null | while read n r v ; do
 	    # We have a dependency.  Make a note that we need the pkgconfig
 	    # tool for this package.
-	    echo "pkgconfig($n)" "$r" "$v"
+	    if  [ -n "$r" ] && [ -n "$v" ]; then
+		echo "pkgconfig($n)" "$r" "$v"
+	    else
+		echo "pkgconfig($n)"
+	    fi
 	done
 	;;
     esac
@@ -36,7 +40,12 @@ case $1 in
 	$pkgconfig --print-requires "$filename" 2> /dev/null | while read n r v ; do
 	    i="`expr $i + 1`"
 	    [ $i -eq 1 ] && echo "pkgconfig"
-	    echo "pkgconfig($n)" "$r" "$v"
+	    [ -n "$n" ] || continue
+	    if  [ -n "$r" ] && [ -n "$v" ]; then
+		echo "pkgconfig($n)" "$r" "$v"
+	    else
+		echo "pkgconfig($n)"
+	    fi
 	done
     esac
     done
