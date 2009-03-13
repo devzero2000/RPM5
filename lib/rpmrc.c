45378b470ac813b2479323a78fe6b3f2842533f9
pkarlsen <pkarlsen>
2009-03-13 17:43:29 +0000

    be sure to check that cvog isn't NULL before freeing it..

--- lib/rpmrc.c
+++ lib/rpmrc.c
@@ -597,8 +597,10 @@ static rpmRC rpmCpuinfo(void)
 	    addMacro(NULL, "_host_vendor", NULL, cvog->vendor, -1);
 	    addMacro(NULL, "_host_os", NULL, cvog->os, -1);
 	}
-	cvog->str = _free(cvog->str);
-	cvog = _free(cvog);
+    	if (cvog) {
+	   cvog->str = _free(cvog->str);
+   	   cvog = _free(cvog);
+	}
 
 	rc = RPMRC_OK;
 	if (rc == RPMRC_OK) {
