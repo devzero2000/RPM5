11d1f42fe19142043a4a453e39a984f9e21610fa
jbj <jbj>
2009-04-30 00:28:17 +0000

    - ruby: stub-in minimal necessary unit test exerciser.

---
+++ ruby/rpm-rb.c
@@ -0,0 +1,1 @@
+static int whatever(void) { return 0; }
---
+++ ruby/rpm-rb.h
@@ -0,0 +1,14 @@
+#ifndef H_RPM_RB
+#define H_RPM_RB
+
+/**
+ * \file ruby/rpm-rb.h
+ */
+
+#include <rpmiotypes.h> 
+#include <rpmio.h>
+
+#include <rpmtypes.h>
+#include <rpmtag.h>
+
+#endif	/* H_RPM_RB */
