009238683bbb187add641ef45803520d3d06486d
jbj <jbj>
2009-05-01 18:27:10 +0000

    - ruby: Ds: add methods.

--- ruby/rpmds-rb.c
+++ ruby/rpmds-rb.c
@@ -54,61 +54,178 @@ fprintf(stderr, "==> %s(0x%lx, 0x%lx)\n", __FUNCTION__, s, v);
     return INT2FIX(_debug = FIX2INT(v));
 }
 
-#ifdef	NOTYET
 static VALUE
-rpmds_rootdir_get(VALUE s)
+rpmds_Count_get(VALUE s)
 {
     void *ptr = rpmds_ptr(s);
     rpmds ds = ptr;
 if (_debug)
 fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
-    return rb_str_new2(rpmdsRootDir(ds));
+    return INT2FIX(rpmdsCount(ds));
 }
 
+/* XXX FIXME: what to call the method? */
 static VALUE
-rpmds_rootdir_set(VALUE s, VALUE v)
-{
-    void *ptr = rpmds_ptr(s);
-    rpmds ds = ptr;
-if (_debug)
-fprintf(stderr, "==> %s(0x%lx, 0x%lx) ptr %p\n", __FUNCTION__, s, v, ptr);
-    rpmdsSetRootDir(ds, StringValueCStr(v));
-    return rb_str_new2(rpmdsRootDir(ds));
-}
-
-static VALUE
-rpmds_vsflags_get(VALUE s)
+rpmds_Type_get(VALUE s)
 {
     void *ptr = rpmds_ptr(s);
     rpmds ds = ptr;
 if (_debug)
 fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
-    return INT2FIX(_debug);
+    return rb_str_new2(rpmdsType(ds));
 }
 
 static VALUE
-rpmds_vsflags_set(VALUE s, VALUE v)
+rpmds_Ix_get(VALUE s)
 {
     void *ptr = rpmds_ptr(s);
     rpmds ds = ptr;
 if (_debug)
-fprintf(stderr, "==> %s(0x%lx, 0x%lx) ptr %p\n", __FUNCTION__, s, v, ptr);
-    rpmdsSetVSFlags(ds, FIX2INT(v));
-    return INT2FIX(rpmdsVSFlags(ds));
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    return INT2FIX(rpmdsIx(ds));
 }
-#endif	/* NOTYET */
+
+static VALUE
+rpmds_Ix_set(VALUE s, VALUE v)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+    int ix = FIX2INT(v);
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    if (ix != rpmdsIx(ds)) {
+	(void) rpmdsSetIx(ds, ix-1);
+	/* XXX flush and recreate N and DNEVR with a rpmdsNext() step */
+        (void) rpmdsNext(ds);
+    }
+    return INT2FIX(rpmdsIx(ds));
+}
+
+static VALUE
+rpmds_BT_get(VALUE s)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    return INT2FIX(rpmdsBT(ds));
+}
+
+static VALUE
+rpmds_BT_set(VALUE s, VALUE v)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+    int BT = FIX2INT(v);
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    (void) rpmdsSetBT(ds, BT);
+    return INT2FIX(rpmdsBT(ds));
+}
+
+static VALUE
+rpmds_Color_get(VALUE s)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    return INT2FIX(rpmdsColor(ds));
+}
+
+static VALUE
+rpmds_Color_set(VALUE s, VALUE v)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+    int color = FIX2INT(v);
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    (void) rpmdsSetColor(ds, color);
+    return INT2FIX(rpmdsColor(ds));
+}
+
+static VALUE
+rpmds_NoPromote_get(VALUE s)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    return INT2FIX(rpmdsNoPromote(ds));
+}
+
+static VALUE
+rpmds_NoPromote_set(VALUE s, VALUE v)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+    int nopromote = FIX2INT(v);
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    (void) rpmdsSetNoPromote(ds, nopromote);
+    return INT2FIX(rpmdsNoPromote(ds));
+}
+
+static VALUE
+rpmds_N_get(VALUE s)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    return rb_str_new2(rpmdsN(ds));
+}
+
+static VALUE
+rpmds_EVR_get(VALUE s)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    return rb_str_new2(rpmdsEVR(ds));
+}
+
+static VALUE
+rpmds_Flags_get(VALUE s)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    return INT2FIX(rpmdsFlags(ds));
+}
+
+static VALUE
+rpmds_DNEVR_get(VALUE s)
+{
+    void *ptr = rpmds_ptr(s);
+    rpmds ds = ptr;
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    return rb_str_new2(rpmdsDNEVR(ds));
+}
 
 static void
 initProperties(VALUE klass)
 {
     rb_define_method(klass, "debug", rpmds_debug_get, 0);
     rb_define_method(klass, "debug=", rpmds_debug_set, 1);
-#ifdef	NOTYET
-    rb_define_method(klass, "rootdir", rpmds_rootdir_get, 0);
-    rb_define_method(klass, "rootdir=", rpmds_rootdir_set, 1);
-    rb_define_method(klass, "vsflags", rpmds_vsflags_get, 0);
-    rb_define_method(klass, "vsflags=", rpmds_vsflags_set, 1);
-#endif	/* NOTYET */
+    rb_define_method(klass, "length", rpmds_Count_get, 0);
+    rb_define_method(klass, "ix", rpmds_Ix_get, 0);
+    rb_define_method(klass, "ix=", rpmds_Ix_set, 1);
+    rb_define_method(klass, "Type", rpmds_Type_get, 0);
+    rb_define_method(klass, "buildtime", rpmds_BT_get, 0);
+    rb_define_method(klass, "buildtime=", rpmds_BT_set, 1);
+    rb_define_method(klass, "color", rpmds_Color_get, 0);
+    rb_define_method(klass, "color=", rpmds_Color_set, 1);
+    rb_define_method(klass, "nopromote", rpmds_NoPromote_get, 0);
+    rb_define_method(klass, "nopromote=", rpmds_NoPromote_set, 1);
+    rb_define_method(klass, "N", rpmds_N_get, 0);
+    rb_define_method(klass, "EVR", rpmds_EVR_get, 0);
+    rb_define_method(klass, "F", rpmds_Flags_get, 0);
+    rb_define_method(klass, "DNEVR", rpmds_DNEVR_get, 0);
 }
 
 /* --- Object ctors/dtors */
--- ruby/tscripts/Ds.rb
+++ ruby/tscripts/Ds.rb
@@ -2,15 +2,31 @@ if ($loglvl)
   print("--> Ds.rb\n");
 end
 
-$RPMTAG_NAME = 1000;
-$N = "popt";
-
+# $ds = Ds.new('rpmlib')
 $ds = Ds.new
 ack("$ds.instance_of?(Ds)", true)
 ack("$ds.kind_of?(Ds)", true)
 ack("$ds.class.to_s", "Ds")
 ack("$ds.debug = 1;", 1);
 ack("$ds.debug = 0;", 0);
+
+ack("$ds.length", 22);
+# FIXME: what to call the method?
+ack("$ds.Type", "Provides");
+ack("$ds.buildtime = 1234;", 1234);
+ack("$ds.color = 4;", 4);
+ack("$ds.nopromote = 1;", 1);
+ack("$ds.nopromote = 0;", 0);
+
+ack("$ds.ix = -1;", -1);
+# ack("$ds.ix++;", -1);
+# ack("++$ds.ix;", 1);
+ack("$ds.ix = 1;", 1);
+
+ack("$ds.N", "BuiltinJavaScript");
+ack("$ds.EVR", "5.2-1");
+ack("$ds.F", 8);
+ack("$ds.DNEVR", "P rpmlib(BuiltinJavaScript) = 5.2-1");
 
 if ($loglvl)
   print("<-- Ds.rb\n");
