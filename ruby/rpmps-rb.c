b02dcbd6362e09089d44ef0414b8302ac86d6237
jbj <jbj>
2009-05-01 17:44:03 +0000

    - ruby: Ps: add methods.

--- ruby/rpmps-rb.c
+++ ruby/rpmps-rb.c
@@ -31,10 +31,42 @@ rpmps_ptr(VALUE s)
 }
 
 /* --- Object methods */
+static VALUE
+rpmps_push(VALUE s)
+{
+    void *ptr = rpmps_ptr(s);
+    rpmps ps = ptr;
+    char *pkgNEVR = "PKG";
+    char *altNEVR = "ALT";
+    char *key = "KEY";
+    rpmProblemType type = RPMPROB_PKG_INSTALLED;
+    char *dn = "/root/";
+    char *bn = "file";
+    unsigned ui = 1234;
+
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    rpmpsAppend(ps, type, pkgNEVR, key, dn, bn, altNEVR, ui);
+    return INT2FIX(rpmpsNumProblems(ps));
+}
+
+static VALUE
+rpmps_print(VALUE s)
+{
+    void *ptr = rpmps_ptr(s);
+    rpmps ps = ptr;
+
+if (_debug)
+fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
+    rpmpsPrint(NULL, ps);
+    return INT2FIX(rpmpsNumProblems(ps));
+}
 
 static void
 initMethods(VALUE klass)
 {
+    rb_define_method(klass, "push", rpmps_push, 7);
+    rb_define_method(klass, "print", rpmps_print, 0);
 }
 
 /* --- Object properties */
@@ -54,17 +86,17 @@ fprintf(stderr, "==> %s(0x%lx, 0x%lx)\n", __FUNCTION__, s, v);
     return INT2FIX(_debug = FIX2INT(v));
 }
 
-#ifdef	NOTYET
 static VALUE
-rpmps_rootdir_get(VALUE s)
+rpmps_length_get(VALUE s)
 {
     void *ptr = rpmps_ptr(s);
     rpmps ps = ptr;
 if (_debug)
 fprintf(stderr, "==> %s(0x%lx) ptr %p\n", __FUNCTION__, s, ptr);
-    return rb_str_new2(rpmpsRootDir(ps));
+    return INT2FIX(rpmpsNumProblems(ps));
 }
 
+#ifdef	NOTYET
 static VALUE
 rpmps_rootdir_set(VALUE s, VALUE v)
 {
@@ -103,6 +135,7 @@ initProperties(VALUE klass)
 {
     rb_define_method(klass, "debug", rpmps_debug_get, 0);
     rb_define_method(klass, "debug=", rpmps_debug_set, 1);
+    rb_define_method(klass, "length", rpmps_length_get, 0);
 #ifdef	NOTYET
     rb_define_method(klass, "rootdir", rpmps_rootdir_get, 0);
     rb_define_method(klass, "rootdir=", rpmps_rootdir_set, 1);
@@ -117,21 +150,13 @@ rpmps_free(rpmps ps)
 {
 if (_debug)
 fprintf(stderr, "==> %s(%p)\n", __FUNCTION__, ps);
-#ifdef	NOTYET
     ps = rpmpsFree(ps);
-#else
-    ps = _free(ps);
-#endif
 }
 
 static VALUE
 rpmps_alloc(VALUE klass)
 {
-#ifdef	NOTYET
-    rpmps ps = NULL;
-#else
-    rpmps ps = xcalloc(1, sizeof(void *));
-#endif
+    rpmps ps = rpmpsCreate();
     VALUE obj = Data_Wrap_Struct(klass, 0, rpmps_free, ps);
 if (_debug)
 fprintf(stderr, "==> %s(0x%lx) obj 0x%lx ps %p\n", __FUNCTION__, klass, obj, ps);
--- ruby/tscripts/Ps.rb
+++ ruby/tscripts/Ps.rb
@@ -2,8 +2,21 @@ if ($loglvl)
   print("--> Ps.rb\n");
 end
 
-$RPMTAG_NAME = 1000;
-$N = "popt";
+$RPMPROB_BADARCH     = 0;
+$RPMPROB_BADOS       = 1;
+$RPMPROB_PKG_INSTALLED = 2;
+$RPMPROB_BADRELOCATE = 3;
+$RPMPROB_REQUIRES    = 4;
+$RPMPROB_CONFLICT    = 5;
+$RPMPROB_NEW_FILE_CONFLICT = 6;
+$RPMPROB_FILE_CONFLICT = 7;
+$RPMPROB_OLDPACKAGE  = 8;
+$RPMPROB_DISKSPACE   = 9;
+$RPMPROB_DISKNODES   = 10;
+$RPMPROB_RDONLY      = 11;
+$RPMPROB_BADPRETRANS = 12;
+$RPMPROB_BADPLATFORM = 13;
+$RPMPROB_NOREPACKAGE = 14;
 
 $ps = Ps.new
 ack("$ps.instance_of?(Ps)", true)
@@ -11,6 +24,35 @@ ack("$ps.kind_of?(Ps)", true)
 ack("$ps.class.to_s", "Ps")
 ack("$ps.debug = 1;", 1);
 ack("$ps.debug = 0;", 0);
+
+$PKG = "PKG";
+$ALT = "ALT";
+$KEY = "/path/pkg";
+$dn = "/root/";
+$bn = "file";
+$ui = 1234;
+$R = "R ALT";
+$C = "C ALT";
+$A = "ARCH/";
+$O = "OS";
+
+ack("$ps.length", 0);
+# ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_BADARCH, $dn, $bn, $ui)", nil);
+# ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_BADOS, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_PKG_INSTALLED, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_BADRELOCATE, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG,   $R, $KEY, $RPMPROB_REQUIRES, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG,   $C, $KEY, $RPMPROB_CONFLICT, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_NEW_FILE_CONFLICT, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_FILE_CONFLICT, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_OLDPACKAGE, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_DISKSPACE, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_DISKNODES, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_RDONLY, $dn, $bn, $ui)", nil);
+# ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_BADPRETRANS, $dn, $bn, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_BADPLATFORM, $A, $O, $ui)", nil);
+ack("$ps.push($PKG, $ALT, $KEY, $RPMPROB_NOREPACKAGE, $dn, $bn, $ui)", nil);
+ack("$ps.length", 12);
 
 if ($loglvl)
   print("<-- Ps.rb\n");
