59aa09ae48f191196b79f688d4be915991aab83b
jbj <jbj>
2009-05-29 15:42:29 +0000

    - create (from ALT/Linux setproctitle-0.3.2),

---
+++ misc/setproctitle.c
@@ -0,0 +1,153 @@
+
+/*
+  Copyright (C) 2004-2006  Dmitry V. Levin <ldv@altlinux.org>
+
+  The setproctitle library interface.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#include "setproctitle.h"
+
+extern int _init(int argc, char *argv[], char *envp[]);
+
+static char *title_buffer = 0;
+static size_t title_buffer_size = 0;
+static char *title_progname, *title_progname_full;
+
+int
+setproctitle(const char *fmt, ...)
+{
+	if (!title_buffer || !title_buffer_size)
+	{
+		errno = ENOMEM;
+		return -1;
+	}
+
+	memset(title_buffer, '\0', title_buffer_size);
+
+	ssize_t written;
+
+	if (fmt)
+	{
+		ssize_t written2;
+		va_list ap;
+
+		written =
+			snprintf(title_buffer, title_buffer_size, "%s: ",
+				 title_progname);
+		if (written < 0 || (size_t) written >= title_buffer_size)
+			return -1;
+
+		va_start(ap, fmt);
+		written2 =
+			vsnprintf(title_buffer + written,
+				  title_buffer_size - written, fmt, ap);
+		va_end(ap);
+		if (written2 < 0
+		    || (size_t) written2 >= title_buffer_size - written)
+			return -1;
+	} else
+	{
+		written =
+			snprintf(title_buffer, title_buffer_size, "%s",
+				 title_progname);
+		if (written < 0 || (size_t) written >= title_buffer_size)
+			return -1;
+	}
+
+	written = strlen(title_buffer);
+	memset(title_buffer + written, '\0', title_buffer_size - written);
+
+	return 0;
+}
+
+/*
+  It has to be _init function, because __attribute__((constructor))
+  functions gets called without arguments.
+*/
+
+int
+_init(int argc, char *argv[], char *envp[])
+{
+	char   *begin_of_buffer = 0, *end_of_buffer = 0;
+	int     i;
+
+	for (i = 0; i < argc; ++i)
+	{
+		if (!begin_of_buffer)
+			begin_of_buffer = argv[i];
+		if (!end_of_buffer || end_of_buffer + 1 == argv[i])
+			end_of_buffer = argv[i] + strlen(argv[i]);
+	}
+
+	for (i = 0; envp[i]; ++i)
+	{
+		if (!begin_of_buffer)
+			begin_of_buffer = envp[i];
+		if (!end_of_buffer || end_of_buffer + 1 == envp[i])
+			end_of_buffer = envp[i] + strlen(envp[i]);
+	}
+
+	if (!end_of_buffer)
+		return 0;
+
+	char  **new_environ = malloc((i + 1) * sizeof(envp[0]));
+
+	if (!new_environ)
+		return 0;
+
+	for (i = 0; envp[i]; ++i)
+		if (!(new_environ[i] = strdup(envp[i])))
+			goto cleanup_enomem;
+	new_environ[i] = 0;
+
+	if (program_invocation_name)
+	{
+		title_progname_full = strdup(program_invocation_name);
+
+		if (!title_progname_full)
+			goto cleanup_enomem;
+
+		char   *p = strrchr(title_progname_full, '/');
+
+		if (p)
+			title_progname = p + 1;
+		else
+			title_progname = title_progname_full;
+
+		program_invocation_name = title_progname_full;
+		program_invocation_short_name = title_progname;
+	}
+
+	environ = new_environ;
+	title_buffer = begin_of_buffer;
+	title_buffer_size = end_of_buffer - begin_of_buffer;
+
+	return 0;
+
+      cleanup_enomem:
+	for (--i; i >= 0; --i)
+		free(new_environ[i]);
+	free(new_environ);
+	return 0;
+}
