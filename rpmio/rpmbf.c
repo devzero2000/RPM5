1c743440cc000696528d71e94af15833eef76ba3
jbj <jbj>
2009-06-26 18:10:33 +0000

    - bf: add an implementation to bang against.

--- rpmio/rpmbf.c
+++ rpmio/rpmbf.c
@@ -7,8 +7,12 @@
 #include <rpmiotypes.h>
 #include <rpmio.h>	/* for *Pool methods */
 #include <rpmlog.h>
+
 #define	_RPMBF_INTERNAL
 #include <rpmbf.h>
+
+#include <rpmhash.h>
+#include <crc.h>
 
 #include "debug.h"
 
@@ -49,7 +53,7 @@ rpmbf rpmbfNew(size_t n, size_t m, size_t k, unsigned flags)
 
     bf->n = (n > 0 ? n : 1024);
     bf->m = (m > 0 ? m : 8192);
-    bf->k = (k > 0 ? k : 1);
+    bf->k = (k > 0 ? k : 2);
     bf->bits = PBM_ALLOC(bf->m);
 
     return rpmbfLink(bf);
@@ -57,16 +61,29 @@ rpmbf rpmbfNew(size_t n, size_t m, size_t k, unsigned flags)
 
 int rpmbfAdd(rpmbf bf, const char * s)
 {
+    rpmuint32_t ix = hashFunctionString(0, s, 0) % bf->m;
+    rpmuint32_t jx = __crc32(0, (const rpmuint8_t *)s, 0) % bf->m;
+    PBM_SET(ix, bf);
+    PBM_SET(jx, bf);
     return 0;
 }
 
 int rpmbfChk(rpmbf bf, const char * s)
 {
-    return 0;
+    rpmuint32_t ix;
+
+    ix = hashFunctionString(0, s, 0) % bf->m;
+    if (!PBM_ISSET(ix, bf))
+	return 0;
+    ix = __crc32(0, (const rpmuint8_t *)s, 0) % bf->m;
+    if (!PBM_ISSET(ix, bf))
+	return 0;
+    return 1;
 }
 
 int rpmbfClr(rpmbf bf)
 {
+    memset(__PBM_BITS(bf), 0, (__PBM_IX(bf->m) + 1) * (__PBM_NBITS/8));
     return 0;
 }
 
