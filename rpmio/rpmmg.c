1071886ff7b886a849cf0e0dbc591467ee53e727
jbj <jbj>
2009-07-17 00:49:41 +0000

    - rpmmg: permit magic for URI's.

--- rpmio/rpmmg.c
+++ rpmio/rpmmg.c
@@ -11,6 +11,7 @@
 #include <rpmiotypes.h>
 #include <rpmio.h>	/* for *Pool methods */
 #include <rpmlog.h>
+#include <rpmurl.h>
 #define	_RPMMG_INTERNAL
 #include <rpmmg.h>
 
@@ -87,13 +88,42 @@ if (_rpmmg_debug)
 fprintf(stderr, "--> rpmmgFile(%p, %s)\n", mg, (fn ? fn : "(nil)"));
 #if defined(HAVE_MAGIC_H)
     if (mg->ms) {
-	t = magic_file(mg->ms, fn);
-	/* XXX HACK: libmagic compiled without <pcreposix.h> spews here. */
-	if (t == NULL) {
-	    const char * msg = magic_error(mg->ms);
-	    if (strstr(msg, "regexec error 17, (match failed)") == NULL)
-		rpmlog(RPMLOG_ERR, _("magic_file(ms, %s) failed: %s\n"),
-			(fn ? fn : "(nil)"), msg);
+	const char * lpath = NULL;
+	int ut = urlPath(fn, &lpath);
+
+	switch (ut) {
+	case URL_IS_FTP:
+	case URL_IS_HKP:
+	case URL_IS_HTTP:
+	case URL_IS_HTTPS:
+	{   char b[512];
+	    size_t nb = 0;
+	    FD_t fd;
+	    
+	    fd = Fopen(fn, "r.ufdio");
+	    if (fd != NULL && !Ferror(fd)) {
+		nb = Fread(b, 1, sizeof(b), fd);
+		(void) Fclose(fd);
+	    }
+	    if (nb > 0)
+		return rpmmgBuffer(mg, b, nb);
+	}   break;
+	case URL_IS_DASH:
+	    break;
+	case URL_IS_PATH:
+	    fn = lpath;
+	    /*@fallthrough@*/
+	case URL_IS_UNKNOWN:
+	default:
+	    t = magic_file(mg->ms, fn);
+	    /* XXX HACK: libmagic compiled without <pcreposix.h> spews here. */
+	    if (t == NULL) {
+		const char * msg = magic_error(mg->ms);
+		if (strstr(msg, "regexec error 17, (match failed)") == NULL)
+		    rpmlog(RPMLOG_ERR, _("magic_file(ms, %s) failed: %s\n"),
+			    (fn ? fn : "(nil)"), msg);
+	    }
+	    break;
 	}
     }
 #endif
