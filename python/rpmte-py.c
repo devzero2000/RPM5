38e53b8b242a5dc7665a7fdfc5f82e7d80c4d58b
jbj <jbj>
2009-11-19 00:03:02 +0000

    - keep HEAD and rpm-5_2 in sync.

--- python/rpmte-py.c
+++ python/rpmte-py.c
@@ -121,7 +121,7 @@ rpmte_R(rpmteObject * s)
     return Py_BuildValue("s", rpmteR(s->te));
 }
 
-#ifdef	RPM_VENDOR_MANDRIVA
+#if defined(RPM_VENDOR_MANDRIVA)
 /*@null@*/
 static PyObject *
 rpmte_D(rpmteObject * s)
@@ -355,13 +355,11 @@ static struct PyMethodDef rpmte_methods[] = {
     {"R",	(PyCFunction)rpmte_R,		METH_NOARGS,
 "te.R() -> R\n\
 - Return element release.\n" },
-
-#ifdef	RPM_VENDOR_MANDRIVA
+#if defined(RPM_VENDOR_MANDRIVA)
     {"D",	(PyCFunction)rpmte_D,		METH_NOARGS,
 "te.D() -> D\n\
 - Return element distepoch.\n" },
 #endif
-
     {"A",	(PyCFunction)rpmte_A,		METH_NOARGS,
 "te.A() -> A\n\
 - Return element arch.\n" },
