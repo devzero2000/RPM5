5359b3fce7e9674f360f8d5d879b01e584b4ab7c
jbj <jbj>
2009-12-09 17:55:01 +0000

    - typo.

--- lib/rpminstall.c
+++ lib/rpminstall.c
@@ -349,7 +349,7 @@ static rpmRC rpmcliEraseElement(rpmts ts, const char * arg)
 	    rc = RPMRC_FAIL;
 	    break;
 	}
-	xx = rpmtsAddEraseElement(ts, h, recOffset);
+	xx = rpmtsAddEraseElement(ts, h, hdrNum);
     }
     mi = rpmmiFree(mi);
 
