14b1d893eff148bd9db72e1c1fc8fa3406d575e5
jbj <jbj>
2010-01-06 07:27:49 +0000

    - js: allow --with-js=/usr/lib:/usr/include/js w external TraceMonkey.

--- debug.h
+++ debug.h
@@ -37,6 +37,13 @@
 #define	VALGRIND_MEMPOOL_EXISTS(pool)	(0)
 
 #define	VALGRIND_HG_CLEAN_MEMORY(_qzz_start, _qzz_len)
+#define	ANNOTATE_HAPPENS_BEFORE(_obj)
+#define	ANNOTATE_HAPPENS_AFTER(_obj)
+#define	ANNOTATE_NEW_MEMORY(_addr, _size)
+#define	ANNOTATE_RWLOCK_CREATE(_lock)
+#define	ANNOTATE_RWLOCK_DESTROY(_lock)
+#define	ANNOTATE_RWLOCK_ACQUIRED(_lock)
+#define	ANNOTATE_RWLOCK_RELEASED(_lock)
 
 #define	CALLGRIND_DUMP_STATS
 #define	CALLGRIND_DUMP_STATS_AT(pos_str)
