0436066b9b23cec86899d5ae4f5b58d2083f49d7
jbj <jbj>
2010-01-20 01:29:59 +0000

    - gsr: add -T,--test options to capture test framework from tjs.c.

--- js/gsr.c
+++ js/gsr.c
@@ -5,10 +5,11 @@
 #include <rpmio.h>
 #include <rpmlog.h>
 #include <argv.h>
-#include <poptIO.h>
 
 #define	_RPMJS_INTERNAL
 #include <rpmjs.h>
+
+#include <rpmcli.h>
 
 #include "debug.h"
 
@@ -26,17 +27,71 @@ extern struct rpmjs_s _rpmgsr;
 static const char * Icode;	/* String with JavaScript program in it */
 static const char * Ifn;	/* Filename with JavaScript program in it */
 static int _debug;		/* 0 = no debug, bigger = more debug */
+static int _loglvl;
+
+/*@unchecked@*/
+static const char * _acknack = "\
+function ack(cmd, expected) {\n\
+  try {\n\
+    actual = eval(cmd);\n\
+  } catch(e) {\n\
+    print(\"NACK:  ack(\"+cmd+\")\tcaught '\"+e+\"'\");\n\
+    return;\n\
+  }\n\
+  if (actual != expected && expected != undefined)\n\
+    print(\"NACK:  ack(\"+cmd+\")\tgot '\"+actual+\"' not '\"+expected+\"'\");\n\
+  else if (loglvl)\n\
+    print(\"       ack(\"+cmd+\")\tgot '\"+actual+\"'\");\n\
+}\n\
+function nack(cmd, expected) {\n\
+  try {\n\
+    actual = eval(cmd);\n\
+  } catch(e) {\n\
+    print(\" ACK: nack(\"+cmd+\")\tcaught '\"+e+\"'\");\n\
+    return;\n\
+  }\n\
+  if (actual == expected)\n\
+    print(\" ACK: nack(\"+cmd+\")\tgot '\"+actual+\"' not '\"+expected+\"'\");\n\
+  else if (loglvl)\n\
+    print(\"      nack(\"+cmd+\")\tgot '\"+actual+\"'\");\n\
+}\n\
+";
 
 /*==============================================================*/
 
 /** GPSEE uses panic() to panic, expects embedder to provide */
+void __attribute__ ((noreturn)) panic(const char *message);
 void __attribute__ ((noreturn)) panic(const char *message)
 {
     rpmlog(RPMLOG_EMERG, "%s: Fatal Error: %s\n", __progname, message);
     exit(1);
 }
 
-/*==============================================================*/
+static void
+rpmjsAppendTest(const char * fn)
+{
+    char * str;
+
+    if (Icode == NULL) {
+	char dstr[32];
+	char lstr[32];
+
+	sprintf(dstr, "%d", _debug);
+	sprintf(lstr, "%d", _loglvl);
+	Icode = rpmExpand(
+		"var debug = ", dstr, ";\n"
+		"var loglvl = ", lstr, ";\n",
+		_acknack, NULL);
+    }
+
+    str = rpmExpand(Icode,
+		"\ntry { require('system').include('", fn,
+		"'); } catch(e) { print(e); print(e.stack); throw e; }", NULL);
+    Icode = _free(Icode);
+    Icode = str;
+
+}
+
 static void rpmgsrArgCallback(poptContext con,
                 /*@unused@*/ enum poptCallbackReason reason,
                 const struct poptOption * opt, /*@unused@*/ const char * arg,
@@ -53,8 +108,17 @@ static void rpmgsrArgCallback(poptContext con,
 	js->flags |= RPMJS_FLAGS_SKIPSHEBANG;
 	Ifn = xstrdup(arg);
 	break;
-    case 'd':	_debug++;	break;
-    case 'z':	_rpmjs_zeal++;	break;
+    case 'T':
+	rpmjsAppendTest(arg);
+	break;
+    case 'd':
+	_debug++;
+	_loglvl = 1;		/* XXX -d imlies loglvl = 1 */
+	_rpmjs_zeal = 2;	/* XXX -d implies GCzeal = 2 */
+	break;
+    case 'z':
+	_rpmjs_zeal++;
+	break;
     default:
 	fprintf(stderr, _("%s: Unknown option -%c\n"), __progname, opt->val);
 	poptPrintUsage(con, stderr, 0);
@@ -71,16 +135,21 @@ static struct poptOption _gsrOptionsTable[] = {
         rpmgsrArgCallback, 0, NULL, NULL },
 /*@=type@*/
 
-  { NULL, 'c', POPT_ARG_STRING,      &Icode, 0,
+  { NULL, 'c', POPT_ARG_STRING,		&Icode, 0,
         N_("Specifies literal JavaScript code to execute"), N_(" code") },
-  { NULL, 'f', POPT_ARG_STRING,      &Ifn, 0,
+  { NULL, 'f', POPT_ARG_STRING,		&Ifn, 0,
         N_("Specifies the filename containing code to run"), N_(" filename") },
-  { NULL, 'F', POPT_ARG_STRING,      NULL, 'F',
+  { NULL, 'F', POPT_ARG_STRING,		NULL, 'F',
         N_("Like -f, but skip shebang if present"), N_(" filename") },
+  { "test", 'T', POPT_ARG_STRING,	NULL, 'T',
+        N_("Like -f, but prepend ack() when including"), N_(" filename") },
+
+    /* XXX non-functional in GPSEE gsr atm */
   { "noexec", 'n', POPT_BIT_SET,	&_rpmgsr.flags, RPMJS_FLAGS_NOEXEC,
 	N_("Engine will load and parse, but not run, the script"), NULL },
+
   { "debug", 'd', POPT_ARG_NONE|POPT_ARGFLAG_DOC_HIDDEN,	NULL, 'd',
-        N_("Increase debuuging"), NULL },
+        N_("Increase debugging"), NULL },
   { "zeal", 'z', POPT_ARG_NONE,	NULL, 'z',
         N_("Increase GC Zealousness"), NULL },
 
@@ -101,8 +170,8 @@ static struct poptOption _optionsTable[] = {
   { NULL, '\0', POPT_ARG_INCLUDE_TABLE, rpmjsIPoptTable, 0,
         N_("JS interpreter options:"), NULL },
 
-  { NULL, '\0', POPT_ARG_INCLUDE_TABLE, rpmioAllPoptTable, 0,
-	N_("Common options for all rpmio executables:"), NULL },
+  { NULL, '\0', POPT_ARG_INCLUDE_TABLE, rpmcliAllPoptTable, 0,
+	N_("Common options for all rpm executables:"), NULL },
 
   POPT_AUTOALIAS
   POPT_AUTOHELP
@@ -126,8 +195,10 @@ int main(int argc, char *argv[])
 
     js->flags = _rpmjs_options | _RPMJS_OPTIONS;
 
-    _rpmio_popt_context_flags = POPT_CONTEXT_POSIXMEHARDER;
-    optCon = rpmioInit(argc, argv, optionsTable);
+#ifdef	NOTYET
+    _rpmcli_popt_context_flags = POPT_CONTEXT_POSIXMEHARDER;
+#endif
+    optCon = rpmcliInit(argc, argv, optionsTable);
 
     /* Add files from CLI. */
     Iargv = (char *const *)poptGetArgs(optCon);
@@ -149,7 +220,12 @@ int main(int argc, char *argv[])
     /* Run JavaScript specified with -c */
     if (Icode) {
 	rc = rpmjsRun(js, Icode, &result);
-	result = NULL;
+
+	if (result != NULL && *result != '\0') {
+	    fprintf(stdout, "%s\n", result);
+	    fflush(stdout);
+	}
+
 	ec = (rc == RPMRC_OK ? 0 : 1);
 	goto finish;
     }
@@ -178,7 +254,7 @@ exit:
     Icode = _free(Icode);
     Ifn = _free(Ifn);
 
-    optCon = rpmioFini(optCon);
+    optCon = rpmcliFini(optCon);
 
     return ec;
 }
