94537e1ab087e4bc2770c34277fb2644f36d4d07
jbj <jbj>
2010-04-08 22:01:49 +0000

    - tests: stub-in a "make check-sql" target.

---
+++ tests/ref/sqldb-basic.sql
@@ -0,0 +1,22 @@
+-- Instantiate the rpmdb.sql schema.
+
+.read ref/rpmdb.sql
+
+-- Basic rpmdbAdd/rpmdbRemove operations using SQL triggers.
+
+BEGIN TRANSACTION;
+INSERT into Packages (h) VALUES ('bing-1.2-3.noarch');
+INSERT into Packages (h) VALUES ('bang-4.5-6.noarch');
+INSERT into Packages (h) VALUES ('boom-7.8.9.noarch');
+COMMIT TRANSACTION;
+
+SELECT * from Packages;
+SELECT * from Nvra;
+
+BEGIN TRANSACTION;
+DELETE FROM Packages WHERE i = 2;
+COMMIT TRANSACTION;
+
+SELECT * from Packages;
+SELECT * from Nvra;
+.exit
