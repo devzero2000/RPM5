db53abba3f9763be2602463f70fc50824178da14
jbj <jbj>
2010-06-20 00:15:52 +0000

    - tests: keep in sync with rpm-5.3 branch.

--- tests/genmanifest.sh
+++ tests/genmanifest.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 wget=/usr/bin/wget
+grep=/bin/grep
+sort=sort
+cut=cut
 
-$wget -O- http://download.idms-linux.org/trunk/i586/groupmap | grep ^base | 
+$wget -O- http://download.idms-linux.org/trunk/i586/groupmap | $grep ^base | 
 while read af; do
-  RPM="`echo $af | cut -d: -f2`"
+  RPM="`echo $af | $cut -d: -f2`"
   echo "http://download.idms-linux.org/trunk/i586/RPMS/$RPM"
-done | sort -u | grep -v 'perl-PAR-Packer'
+done | $sort -u | $grep -v 'perl-PAR-Packer'
 
