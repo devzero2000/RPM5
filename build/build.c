e1410932f3b101bcd27394a39a002580a222c169
jbj <jbj>
2010-06-21 01:31:31 +0000

    - add the DSA pubkey/signature to all packages produced by rpmbuild.

--- build/build.c
+++ build/build.c
@@ -9,6 +9,10 @@
 #include <rpmcb.h>
 #include <rpmsq.h>
 
+#define	_RPMBC_INTERNAL
+#include <rpmbc.h>
+#include <rpmhkp.h>		/* XXX _rpmhkp_debug */
+
 #define	_RPMTAG_INTERNAL
 #include <rpmbuild.h>
 #include "signature.h"		/* XXX rpmTempFile */
@@ -332,6 +336,81 @@ exit:
     return rc;
 }
 
+static int rpmbcExportPubkey(pgpDig dig)
+{
+    uint8_t pkt[8192];
+uint8_t * be = pkt;
+    size_t pktlen;
+    time_t now = time(NULL);
+    uint32_t bt = now;
+    uint16_t bn;
+pgpDigParams pubp = pgpGetPubkey(dig);
+rpmbc bc = dig->impl;
+int xx;
+
+    *be++ = 0x80 | (PGPTAG_PUBLIC_KEY << 2) | 0x01;
+    be += 2;
+
+    *be++ = 0x04;
+    *be++ = (bt >> 24);
+    *be++ = (bt >> 16);
+    *be++ = (bt >>  8);
+    *be++ = (bt      );
+    *be++ = pubp->pubkey_algo;
+
+#ifdef	DYING
+fprintf(stderr, "\t p: "),  mpfprintln(stderr, bc->dsa_keypair.param.p.size, bc->dsa_keypair.param.p.modl);
+#endif
+    bn = MP_WORDS_TO_BITS(bc->dsa_keypair.param.p.size);
+    *be++ = (bn >> 8);	*be++ = (bn     );
+    xx = i2osp(be, bn/8, bc->dsa_keypair.param.p.modl, bc->dsa_keypair.param.p.size);
+    be += bn/8;
+
+#ifdef	DYING
+fprintf(stderr, "\t q: "),  mpfprintln(stderr, bc->dsa_keypair.param.q.size, bc->dsa_keypair.param.q.modl);
+#endif
+    bn = MP_WORDS_TO_BITS(bc->dsa_keypair.param.q.size);
+    *be++ = (bn >> 8);	*be++ = (bn     );
+    xx = i2osp(be, bn/8, bc->dsa_keypair.param.q.modl, bc->dsa_keypair.param.q.size);
+    be += bn/8;
+
+#ifdef	DYING
+fprintf(stderr, "\t g: "),  mpfprintln(stderr, bc->dsa_keypair.param.g.size, bc->dsa_keypair.param.g.data);
+#endif
+    bn = MP_WORDS_TO_BITS(bc->dsa_keypair.param.g.size);
+    *be++ = (bn >> 8);	*be++ = (bn     );
+    xx = i2osp(be, bn/8, bc->dsa_keypair.param.g.data, bc->dsa_keypair.param.g.size);
+    be += bn/8;
+
+#ifdef	DYING
+fprintf(stderr, "\t y: "),  mpfprintln(stderr, bc->dsa_keypair.y.size, bc->dsa_keypair.y.data);
+#endif
+    bn = MP_WORDS_TO_BITS(bc->dsa_keypair.y.size);
+    *be++ = (bn >> 8);	*be++ = (bn     );
+    xx = i2osp(be, bn/8, bc->dsa_keypair.y.data, bc->dsa_keypair.y.size);
+    be += bn/8;
+
+    pktlen = (be - pkt);
+    bn = pktlen - 3;
+    pkt[1] = (bn >> 8);
+    pkt[2] = (bn     );
+
+    xx = pgpPubkeyFingerprint(pkt, pktlen, pubp->signid);
+
+    dig->pub = memcpy(xmalloc(pktlen), pkt, pktlen);
+    dig->publen = pktlen;
+
+if (_rpmhkp_debug) {
+    pgpDig ndig = pgpDigNew(0);
+fprintf(stderr, "==========\n%s\n==========\n", pgpHexStr(dig->pub, dig->publen));
+    xx = pgpPrtPkts(dig->pub, dig->publen, ndig, -1);
+    ndig = pgpDigFree(ndig);
+fprintf(stderr, "<-- %s: %s\n", __FUNCTION__, pgpHexStr(pubp->signid, sizeof(pubp->signid)));
+}
+
+    return 0;
+}
+
 rpmRC buildSpec(rpmts ts, Spec spec, int what, int test)
 {
     rpmRC rc = RPMRC_OK;
@@ -345,6 +424,10 @@ rpmRC buildSpec(rpmts ts, Spec spec, int what, int test)
 	pubp->pubkey_algo = PGPPUBKEYALGO_DSA;
 	xx = pgpImplGenerate(dig);
 assert(xx == 1);
+
+	if (pgpImplVecs == &rpmbcImplVecs)
+	    xx = rpmbcExportPubkey(dig);
+
 	spec->dig = dig;
     }
 
