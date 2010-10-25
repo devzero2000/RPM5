57664c0c9f8f10b7cf7e35599754cee97c49f7f8
jbj <jbj>
2010-10-25 05:27:30 +0000

    - indiana: portability fiddle ups.

--- tools/rpmmtree.c
+++ tools/rpmmtree.c
@@ -984,7 +984,7 @@ strunvis(char * dst, const char * src)
 /*==============================================================*/
 
 /* XXX *BSD systems already have getmode(3) and setmode(3) */
-#if defined(__linux__) || defined(__LCLINT__) || defined(__QNXNTO__)
+#if defined(__linux__) || defined(__sun__) || defined(__LCLINT__) || defined(__QNXNTO__)
 #if !defined(HAVE_GETMODE) || !defined(HAVE_SETMODE)
 
 #define	SET_LEN	6		/* initial # of bitcmd struct to malloc */
