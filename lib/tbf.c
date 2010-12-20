70e35ba4c428288edc22b655901233576e6b4fbf
jbj <jbj>
2010-12-20 21:11:54 +0000

    - tbf: add per-package overhead.

--- lib/tbf.c
+++ lib/tbf.c
@@ -34,7 +34,20 @@ static unsigned rpmbfWrite(FD_t fd, rpmbf * bfa, size_t npkgs)
 
     for (ix = 0; ix < (int) npkgs; ix++) {
 	rpmbf bf = bfa[ix];
-	xx = Fwrite(bf->bits, 1, (bf->m+7)/8, fd);
+	static const char _bfmagic[] = "BFL3";
+	const unsigned char * _bits = (bf ? bf->bits : NULL);
+	uint32_t _n = (bf ? bf->n : 0);
+	uint32_t _m = (bf ? bf->m : 0);
+	uint32_t _k = (bf ? bf->k : 0);
+
+	if (rpmIsVerbose())
+	    fprintf(stderr, "\tbf[%d] %p n %u m %u k %u\n", ix, bf, _n, _m, _k);
+	xx = Fwrite(_bfmagic, 1, 2, fd);
+	xx = Fwrite(&_n, 1, sizeof(_n), fd);
+	xx = Fwrite(&_m, 1, sizeof(_m), fd);
+	xx = Fwrite(&_k, 1, sizeof(_m), fd);
+	if (_m > 0)
+	    xx = Fwrite(_bits, 1, (_m+7)/8, fd);
     }
     xx = Fclose(fd);
     xx = Stat(fn, &sb);
@@ -142,39 +155,43 @@ int xx;
     ix = 0;
     while ((rc = rpmgiNext(gi)) == RPMRC_OK) {
 	static const char newline[] = "\n";
-	Header h;
 	rpmbf bf;
-#ifdef	NOTYET
-	n = (he->c + 31) & ~0x1f;
-	rpmbfParams(n, e, &m, &k);
-#endif
-	bfa[ix++] = bf = rpmbfNew(m, k, 0);
-	nb += (m+7)/8;
+	Header h;
+
+	bf = NULL;
 	h = rpmgiHeader(gi);
 	he->tag = hetag;
-	if (headerGet(h, he, 0))
-	for (jx = 0; jx < (int) he->c; jx++) {
-	    const char * s = he->p.argv[jx];
-	    size_t ns = strlen(s);
-	    rpmbfAdd(bf, s, ns);
-	    xx = Fwrite(s, 1, ns, fd);
-	    xx = Fwrite(newline, 1, 1, fd);
-	    ndirs++;
-	    ndirb += ns + 1;
+	if (headerGet(h, he, 0) && he->c > 0) {
+	    /* XXX make sure n >= _nmin in rpmbfParams() */
+	    n = (he->c > 10 ? he->c : 10);
+	    rpmbfParams(n, e, &m, &k);
+	    nb += (m+7)/8;
+	    bf = rpmbfNew(m, k, 0);
+	    for (jx = 0; jx < (int) he->c; jx++) {
+		const char * s = he->p.argv[jx];
+		size_t ns = strlen(s);
+		xx = rpmbfAdd(bf, s, ns);
+		xx = Fwrite(s, 1, ns, fd);
+		xx = Fwrite(newline, 1, 1, fd);
+		ndirs++;
+		ndirb += ns + 1;
+	    }
 	}
 	xx = Fwrite(newline, 1, 1, fd);
 	ndirb++;
+	bfa[ix++] = bf;
     }
     xx = Fclose(fd);
     xx = Stat(fn_dirs, &sb);
     xx = Unlink(fn_dirs);
     ndirxz = sb.st_size;
 
-fprintf(stdout, "Dirnames: %u bytes (%u items)\n", (unsigned) ndirb, ndirs);
+fprintf(stdout, "       npkgs: %u\n", (unsigned) npkgs);
+fprintf(stdout, "    Dirnames: %u bytes (%u items)\n", (unsigned) ndirb, ndirs);
 fprintf(stdout, "  with XZDIO: %u bytes\n", (unsigned) ndirxz);
 
-fprintf(stdout, "Bloom filter: {%u,%u}[%u] false positives: %5.2g\n", (unsigned)n, (unsigned)m, (unsigned)k, e);
-fprintf(stdout, "Uncompressed: %u = %u * %u\n", (unsigned)nb, (unsigned)npkgs, (unsigned)(m + 7)/8);
+fprintf(stdout, "Bloom filter: false positives: %5.2g\n", e);
+fprintf(stdout, "Uncompressed: %u bytes\n", (unsigned)nb);
 fprintf(stdout, "  with XZDIO: %u bytes\n", rpmbfWrite(Fopen(fn_bf, fmode_w), bfa, npkgs));
  
     ec = EXIT_SUCCESS;
