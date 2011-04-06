319f564ac04888c3b55bf09df27b406fdeb5f25e
jbj <jbj>
2011-04-06 23:34:35 +0000

    - build: fix: resurrect %description -l XY yet again.

--- build/parseDescription.c
+++ build/parseDescription.c
@@ -34,7 +34,7 @@ int parseDescription(Spec spec)
 	/*@modifies name, lang @*/
 {
     rpmParseState nextPart = (rpmParseState) RPMRC_FAIL; /* assume error */
-    rpmiob iob;
+    rpmiob iob = NULL;
     int flag = PART_SUBNAME;
     Package pkg;
     int rc, argc;
@@ -54,7 +54,7 @@ int parseDescription(Spec spec)
     if ((rc = poptParseArgvString(spec->line, &argc, &argv))) {
 	rpmlog(RPMLOG_ERR, _("line %d: Error parsing %%description: %s\n"),
 		 spec->lineNum, poptStrerror(rc));
-	return RPMRC_FAIL;
+	goto exit;
     }
 
     name = NULL;
@@ -90,8 +90,8 @@ int parseDescription(Spec spec)
     }
 
     /* Lose the inheirited %description (if present). */
-#if !defined(RPM_VENDOR_MANDRIVA) /* mdvbz#62979 */
-    {	HE_t he = memset(alloca(sizeof(*he)), 0, sizeof(*he));
+    if (spec->packages->header != pkg->header) {
+	HE_t he = memset(alloca(sizeof(*he)), 0, sizeof(*he));
 	int xx;
 	he->tag = RPMTAG_DESCRIPTION;
 	xx = headerGet(pkg->header, he, 0);
@@ -99,7 +99,6 @@ int parseDescription(Spec spec)
 	if (xx && he->t == RPM_STRING_TYPE)
 	    xx = headerDel(pkg->header, he, 0);
     }
-#endif
     
     t = stashSt(spec, pkg->header, RPMTAG_DESCRIPTION, lang);
     
@@ -107,16 +106,17 @@ int parseDescription(Spec spec)
 
     if ((rc = readLine(spec, STRIP_TRAILINGSPACE | STRIP_COMMENTS)) > 0) {
 	nextPart = PART_NONE;
-    } else {
-	if (rc) {
+	goto exit;
+    }
+    if (rc < 0) {
 	    nextPart = (rpmParseState) RPMRC_FAIL;
 	    goto exit;
 	}
+
 	while ((nextPart = isPart(spec)) == PART_NONE) {
 	    iob = rpmiobAppend(iob, spec->line, 1);
 	    if (t) t->t_nlines++;
-	    if ((rc =
-		readLine(spec, STRIP_TRAILINGSPACE | STRIP_COMMENTS)) > 0) {
+	if ((rc = readLine(spec, STRIP_TRAILINGSPACE | STRIP_COMMENTS)) > 0) {
 		nextPart = PART_NONE;
 		break;
 	    }
@@ -125,17 +125,15 @@ int parseDescription(Spec spec)
 		goto exit;
 	    }
 	}
-    }
     
     iob = rpmiobRTrim(iob);
     if (!(noLang && strcmp(lang, RPMBUILD_DEFAULT_LANG))) {
-	(void) headerAddI18NString(pkg->header, RPMTAG_DESCRIPTION,
-			rpmiobStr(iob), lang);
+	const char * s = rpmiobStr(iob);
+	(void) headerAddI18NString(pkg->header, RPMTAG_DESCRIPTION, s, lang);
     }
     
-    iob = rpmiobFree(iob);
-     
 exit:
+    iob = rpmiobFree(iob);
     argv = _free(argv);
     optCon = poptFreeContext(optCon);
     return nextPart;
