04fb1fccfe85eb1784de9e0a7c59361a303a81af
pkarlsen <pkarlsen>
2011-07-06 12:55:29 +0000

    dbconvert: fix segfault when no root is specified.

--- tools/dbconvert.c
+++ tools/dbconvert.c
@@ -114,7 +114,7 @@ rpmdb_convert(const char *prefix, int dbtype, int swap, int rebuild)
 	     "create mpool txn thread thread_count=64 nofsync", -1);
 
     /* (ugly) clear any existing locks */
-    fn = rpmGetPath(prefix[0] ? prefix : "", dbpath, "/", "__db.*", NULL);
+    fn = rpmGetPath(prefix && prefix[0] ? prefix : "", dbpath, "/", "__db.*", NULL);
     xx = Glob(fn, 0, NULL, &gl);
     for (i = 0; i < (int) gl.gl_pathc; i++)
 	xx = Unlink(gl.gl_pathv[i]);
@@ -377,7 +377,7 @@ rpmdb_convert(const char *prefix, int dbtype, int swap, int rebuild)
     fn = _free(fn);
 
     /* clear locks */
-    fn = rpmGetPath(prefix[0] ? prefix : "", dbpath, "/", "__db.*", NULL);
+    fn = rpmGetPath(prefix && prefix[0] ? prefix : "", dbpath, "/", "__db.*", NULL);
     xx = Glob(fn, 0, NULL, &gl);
     for (i = 0; i < (int) gl.gl_pathc; i++)
 	xx = Unlink(gl.gl_pathv[i]);
