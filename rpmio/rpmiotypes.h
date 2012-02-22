dd436cee85feca70c04d3157082ebadfeb12981e
jbj <jbj>
2012-02-22 00:12:11 +0000

    - add RPMRC_NOSIG error return.

--- rpmio/rpmiotypes.h
+++ rpmio/rpmiotypes.h
@@ -13,7 +13,8 @@ typedef	enum rpmRC_e {
     RPMRC_NOTFOUND	= 1,	/*!< Generic not found code. */
     RPMRC_FAIL		= 2,	/*!< Generic failure code. */
     RPMRC_NOTTRUSTED	= 3,	/*!< Signature is OK, but key is not trusted. */
-    RPMRC_NOKEY		= 4	/*!< Public key is unavailable. */
+    RPMRC_NOKEY		= 4,	/*!< Public key is unavailable. */
+    RPMRC_NOSIG		= 5,	/*!< Signature is unavailable. */
 } rpmRC;
 
 /** \ingroup rpmio
