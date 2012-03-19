ae502b445cec7c7db7e73b4ada14e5627473460a
jbj <jbj>
2012-03-19 02:08:35 +0000

    - odbc: use C99 trickey w static arrays & anonymous pointers using C macros.

--- rpmio/todbc.c
+++ rpmio/todbc.c
@@ -102,37 +102,106 @@ SPEW(0, rc, odbc);
 }
 
 /*==============================================================*/
-static const char *const _odbc_stmts[] = {
-    "SHOW DATABASES;",
-    "USE test;",
-    "SHOW TABLES;",
-    "DROP TABLE Packages;",
+typedef struct _PARAM_s * _PARAM_t;
+
+struct _PARAM_s {
+    SQLUSMALLINT ParameterNumber;
+    SQLSMALLINT ValueType;
+    SQLSMALLINT ParameterType;
+    SQLULEN LengthPrecision;
+    SQLSMALLINT ParameterScale;
+    SQLPOINTER ParameterValue;
+    SQLLEN * Strlen_or_Ind;
+};
+    
+typedef struct _STMT_s * _STMT_t;
+struct _STMT_s {
+    const char * sql;
+    _PARAM_t * params;
+};
+
+/* XXX c99 only */
+#define	_mkSTMT(_sql, _params)	\
+  &(struct _STMT_s) { .sql = _sql, .params = _params }
+
+#ifdef	 NOTYET		/* XXX todo++ */
+#define	_mkP1(_chararray) \
+  &(_PARAM_t) { \
+    &(struct _PARAM_s) { \
+	.ParameterNumber = 1, \
+	.ValueType = SQL_C_CHAR, \
+	.ParameterType = SQL_CHAR, \
+	.LengthPrecision = sizeof(_chararray), \
+	.ParameterScale = 0, \
+	.ParameterValue = _chararray, \
+	.Strlen_or_Ind = NULL, \
+    }, \
+  }
+#endif
+
+static _STMT_t _odbc_stmts[] = {
+  _mkSTMT("SHOW DATABASES", NULL),
+  _mkSTMT("USE test", NULL),
+  _mkSTMT("SHOW TABLES", NULL),
+  _mkSTMT("DROP TABLE Packages IF EXISTS", NULL),
+  _mkSTMT(
 "CREATE TABLE Packages (\n\
     i INTEGER PRIMARY KEY NOT NULL,\n\
     Nvra VARCHAR(64)\n\
-);",
-    "DESCRIBE Packages;",
-    "INSERT INTO Packages VALUES( 1, 'Bing' );",
-    "INSERT INTO Packages VALUES( 2, 'Bang' );",
-    "INSERT INTO Packages VALUES( 3, 'Boom' );",
-    "SELECT * from Packages;",
-    "DROP TABLE Packages;",
-    NULL
+)", NULL),
+  _mkSTMT("DESCRIBE Packages", NULL),
+  _mkSTMT("INSERT INTO Packages VALUES( 1, 'Bing' )", NULL),
+  _mkSTMT("INSERT INTO Packages VALUES( 2, 'Bang' )", NULL),
+  _mkSTMT("INSERT INTO Packages VALUES( 3, 'Boom' )", NULL),
+  _mkSTMT("SELECT * FROM Packages", NULL),
+  _mkSTMT("DROP TABLE Packages", NULL),
+  NULL
 };
 
-static int odbcRun(ODBC_t odbc, const char *const stmts[], void * _fp)
+#undef	_mkSTMT
+
+static int odbcBind(ODBC_t odbc, _PARAM_t param)
+{
+    int rc = CHECK(odbc, "SQLBindParam",
+		SQLBindParam(odbc->stmt,
+			param->ParameterNumber,
+			param->ValueType,
+			param->ParameterType,
+			param->LengthPrecision,
+			param->ParameterScale,
+			param->ParameterValue,
+			param->Strlen_or_Ind));
+
+SPEW(0, rc, odbc);
+    return rc;
+}
+
+static int odbcStmt(ODBC_t odbc, _STMT_t stmt, void * _fp)
 {
     FILE * fp = (_fp ? _fp : stderr);
-    const char * s;
     int rc = -1;
-    int i;
+    const char * s = stmt->sql;
+    _PARAM_t * params = stmt->params;
 
-    for (i = 0; (s = stmts[i]) != NULL; i++) {
 fprintf(fp, "==> %s\n", s);
-	rc = odbcPrepare(odbc, s, 0);
-	rc = odbcExecute(odbc);
-	rc = odbcPrint(odbc, fp);
-    }
+    rc = odbcPrepare(odbc, s, 0);
+
+    while (params && *params)
+	rc = odbcBind(odbc, *params++);
+
+    rc = odbcExecute(odbc);
+    rc = odbcPrint(odbc, fp);
+
+SPEW(0, rc, odbc);
+    return rc;
+}
+
+static int odbcRun(ODBC_t odbc, _STMT_t * stmts, void * _fp)
+{
+    int rc = -1;
+
+    while (stmts && *stmts)
+	rc = odbcStmt(odbc, *stmts++, _fp);
 
 SPEW(0, rc, odbc);
     return rc;
