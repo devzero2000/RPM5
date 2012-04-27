d528920d42784bd7c0df7bfa7b1915d9aed29b00
jbj <jbj>
2012-04-27 01:39:08 +0000

    - sqldb: enhance the schgmea, add inner joing and equi-join access examples.

--- tests/ref/rpmdb.sql
+++ tests/ref/rpmdb.sql
@@ -1,17 +1,57 @@
+DROP TABLE IF EXISTS Packages;
 CREATE TABLE Packages (
-  i	INTEGER UNIQUE PRIMARY KEY NOT NULL,
-  h	BLOB NOT NULL
+  v	INTEGER UNIQUE PRIMARY KEY NOT NULL,
+  k	BLOB NOT NULL
 );
-CREATE TEMP TRIGGER insert_Packages AFTER INSERT ON Packages
+
+CREATE TRIGGER insert_Packages AFTER INSERT ON Packages
   BEGIN
-    INSERT INTO Nvra (k,v)	VALUES ( new.h, new.rowid );
+    INSERT INTO Nvra (k,v)	VALUES (
+	new.k, new.rowid );
+    INSERT INTO Name (k,v)	VALUES (
+	SUBSTR(new.k,  1, 4), new.rowid );
+    INSERT INTO Version (k,v)	VALUES (
+	SUBSTR(new.k,  6, 3), new.rowid );
+    INSERT INTO Release (k,v)	VALUES (
+	SUBSTR(new.k, 10, 1), new.rowid );
+    INSERT INTO Arch (k,v)	VALUES (
+	SUBSTR(new.k, 12), new.rowid );
   END;
-CREATE TEMP TRIGGER delete_Packages BEFORE DELETE ON Packages
+CREATE TRIGGER delete_Packages BEFORE DELETE ON Packages
   BEGIN
-    DELETE FROM Nvra WHERE v = old.rowid;
+    DELETE FROM Nvra	WHERE v = old.rowid;
+    DELETE FROM Name	WHERE v = old.rowid;
+    DELETE FROM Version	WHERE v = old.rowid;
+    DELETE FROM Release	WHERE v = old.rowid;
+    DELETE FROM Arch	WHERE v = old.rowid;
   END;
 
+DROP TABLE IF EXISTS Nvra;
 CREATE TABLE Nvra (
-  k	TEXT PRIMARY KEY NOT NULL,
-  v	INTEGER REFERENCES Packages(i) ON UPDATE RESTRICT ON DELETE RESTRICT
+  k	TEXT NOT NULL,
+  v	INTEGER REFERENCES Packages
+);
+
+DROP TABLE IF EXISTS Name;
+CREATE TABLE Name (
+  k	TEXT NOT NULL,
+  v	INTEGER REFERENCES Packages
+);
+
+DROP TABLE IF EXISTS Version;
+CREATE TABLE Version (
+  k	TEXT NOT NULL,
+  v	INTEGER REFERENCES Packages
+);
+
+DROP TABLE IF EXISTS Release;
+CREATE TABLE Release (
+  k	TEXT NOT NULL,
+  v	INTEGER REFERENCES Packages
+);
+
+DROP TABLE IF EXISTS Arch;
+CREATE TABLE Arch (
+  k	TEXT NOT NULL,
+  v	INTEGER REFERENCES Packages
 );
--- tests/ref/sqldb-basic.sql
+++ tests/ref/sqldb-basic.sql
@@ -13,24 +13,48 @@
 .show
 
 -- Instantiate the rpmdb.sql schema.
+PRAGMA foreign_keys = ON;
 .read ref/rpmdb.sql
 
 -- Basic rpmdbAdd/rpmdbRemove operations using SQL triggers.
 BEGIN TRANSACTION;
-INSERT into Packages (h) VALUES ('bing-1.2-3.noarch');
-INSERT into Packages (h) VALUES ('bang-4.5-6.noarch');
-INSERT into Packages (h) VALUES ('boom-7.8.9.noarch');
+INSERT into Packages (k) VALUES ('bing-1.2-3.noarch');
+INSERT into Packages (k) VALUES ('bang-4.5-6.noarch');
+INSERT into Packages (k) VALUES ('boom-7.8-9.noarch');
 COMMIT TRANSACTION;
 
 SELECT * from Packages;
 SELECT * from Nvra;
+SELECT * from Name;
+SELECT * from Version;
+SELECT * from Release;
+SELECT * from Arch;
+
+-- explicit inner join
+SELECT Arch.k, Packages.k
+  FROM Arch
+  INNER JOIN Packages ON Arch.v = Packages.v;
+
+-- implicit inner join
+SELECT Arch.k, Packages.k
+  FROM Arch, Packages
+  WHERE Arch.v = Packages.v;
+
+-- equi-join
+SELECT *
+  FROM Arch
+  INNER JOIN Packages USING (v);
 
 BEGIN TRANSACTION;
-DELETE FROM Packages WHERE i = 2;
+DELETE FROM Packages WHERE v = 2;
 COMMIT TRANSACTION;
 
 SELECT * from Packages;
 SELECT * from Nvra;
+SELECT * from Name;
+SELECT * from Version;
+SELECT * from Release;
+SELECT * from Arch;
 
 -- Basic .foo SQL metadata tests.
 .backup main tmp/main.bak
