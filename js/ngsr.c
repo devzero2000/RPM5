b150dd7923dfb0139ee25a8d65859e14007fc723
jbj <jbj>
2012-05-27 19:18:37 +0000

    - ngsr: mimic GPSEE gsr sufficiently well to re-use GPSEE "make check" tests.

--- js/ngsr.c
+++ js/ngsr.c
@@ -24,6 +24,7 @@ extern const char * __progname;
 static const char * Icode;	/* String with JavaScript program in it */
 static const char * Ifn;	/* Filename with JavaScript program in it */
 static int verbosity;		/* 0 = no debug, bigger = more debug */
+static int gcZeal;
 
 /*==============================================================*/
 
@@ -51,6 +52,12 @@ static void rpmjsArgCallback(poptContext con,
 	js->flags |= RPMJS_FLAGS_SKIPSHEBANG;
 	Ifn = xstrdup(arg);
 	break;
+    case 'z':
+        gcZeal++;
+        break;
+    case 'd':
+        rpmIncreaseVerbosity();
+        break;
     default:
 	fprintf(stderr, _("%s: Unknown option -%c\n"), __progname, opt->val);
 	poptPrintUsage(con, stderr, 0);
@@ -75,6 +82,10 @@ static struct poptOption _gsrOptionsTable[] = {
         N_("Display this help"), NULL },
   { "noexec", 'n', POPT_BIT_SET,	&_rpmjs.flags, RPMJS_FLAGS_NOEXEC,
 	N_("Engine will load and parse, but not run, the script"), NULL },
+  { NULL, 'z', 0, NULL, (int)'z',
+	N_("Increase GC Zealousness"), NULL},
+  { NULL, 'd', 0, NULL, (int)'d',
+	N_("Provide more detailed output"), NULL},
 
   POPT_TABLEEND
 };
@@ -87,10 +98,10 @@ static struct poptOption _optionsTable[] = {
         rpmjsArgCallback, 0, NULL, NULL },
 /*@=type@*/
 
-  { NULL, '\0', POPT_ARG_INCLUDE_TABLE, _gsrOptionsTable, 0,
+ { NULL, '\0', POPT_ARG_INCLUDE_TABLE, _gsrOptionsTable, 0,
         N_("Command options:"), NULL },
 
-  { NULL, '\0', POPT_ARG_INCLUDE_TABLE, rpmjsIPoptTable, 0,
+ { NULL, '\0', POPT_ARG_INCLUDE_TABLE, rpmjsIPoptTable, 0,
         N_("JS interpreter options:"), NULL },
 
  { NULL, '\0', POPT_ARG_INCLUDE_TABLE, rpmioAllPoptTable, 0,
@@ -103,11 +114,52 @@ static struct poptOption _optionsTable[] = {
 
 static struct poptOption *optionsTable = &_optionsTable[0];
 /*==============================================================*/
+static int _gsrExitCode(rpmjs js)
+{
+    gpsee_interpreter_t *I = js->I;
+    const char *reason = NULL;
+    int ec = 1;
 
-int main(int argc, char *argv[])
+    if (I->grt->exitType & et_successMask) {
+	ec = I->grt->exitCode;
+	goto exit;
+    }
+
+    switch (I->grt->exitType) {
+    case et_successMask:
+    case et_errorMask:
+    case et_requested:
+    case et_finished:
+    case et_dummy:
+    default:
+	reason = N_("impossible");
+	GPSEE_NOT_REACHED(reason);
+	break;
+    case et_execFailure:
+	reason = N_("execFailure - probable native module error, returning JS_FALSE without setting exception");
+	break;
+    case et_compileFailure:
+	reason = N_("script could not be compiled");
+	break;
+    case et_unknown:
+	reason = N_("unknown - probable native module error, returning JS_FALSE without setting exception");
+	break;
+    case et_exception:
+	reason = NULL;
+	goto exit;
+	/*@notreached@*/ break;
+    }
+
+    rpmlog(RPMLOG_NOTICE, _("Unexpected interpreter shutdown: %s(%m)"), reason);
+
+exit:
+    return ec;
+}
+
+static int _main(int argc, char *argv[])
 {
     poptContext optCon;
-    rpmjs js;
+    rpmjs js = NULL;
     char *const * Iargv = NULL;	/* Becomes arguments array in JS program */
     const char * result = NULL;
     int ac = 0;
@@ -165,6 +217,20 @@ JSDContext *jsdc;
     if (gpsee_verbosity(0) < verbosity)
 	gpsee_setVerbosity(verbosity);
 
+    /* XXX set from macro? */
+#ifdef	REFERENCE
+#ifdef JSFEATURE_GC_ZEAL
+    if (JS_HasFeature(JSFEATURE_GC_ZEAL) == JS_TRUE)
+	JS_SetGCZeal(I->cx, gcZeal);
+#else
+# ifdef JS_GC_ZEAL
+    JS_SetGCZeal(I->cx, gcZeal);
+# else
+#  warning JS_SetGCZeal not available when building with this version of SpiderMonkey (try a debug build?)
+# endif
+#endif
+#endif	/* REFERENCE */
+
     /* Run JavaScript specified with -c */
     if (Icode) {
 	const char * result = NULL;
@@ -198,6 +264,7 @@ JSDContext *jsdc;
 	    if (!gpsee_compileScript(I->cx, preloadfn,
 			NULL, NULL, &script, I->realm->globalObject, &scrobj))
 	    {
+		I->grt->exitType = et_compileFailure;
 		rpmlog(RPMLOG_EMERG,
 			  "%s: Unable to compile preload script '%s'\n",
 			  __progname, preloadfn);
@@ -211,8 +278,10 @@ JSDContext *jsdc;
 
 	    if (!F_ISSET(js->flags, NOEXEC)) {
 		JS_AddNamedObjectRoot(I->cx, &scrobj, "preload_scrobj");
+		I->grt->exitType = et_execFailure;
 		if (JS_ExecuteScript(I->cx, I->realm->globalObject, script, &v) == JS_FALSE)
-		{
+		    I->grt->exitType = et_finished;
+		else {
 		    if (JS_IsExceptionPending(I->cx))
 			I->grt->exitType = et_exception;
 		    JS_ReportPendingException(I->cx);
@@ -231,25 +300,48 @@ JSDContext *jsdc;
     JS_SetOptions(I->cx, JS_GetOptions(I->cx) | JSOPTION_DONT_REPORT_UNCAUGHT);
 
     if (Ifn == NULL) {
-	ec = Icode ? 0 : 1;
+	if (Icode == NULL)
+	    goto finish;	/* XXX gsr does usage() here */
+
+	if (I->grt->exitType == et_unknown)
+	    I->grt->exitType = et_finished;
 	goto finish;
     }
 
     /* Run (pre-compiled) JavaScript specified with -f */
     if (ac != 0)	/* XXX FIXME */
-	    js->flags |= RPMJS_FLAGS_SKIPSHEBANG;
+	js->flags |= RPMJS_FLAGS_SKIPSHEBANG;
 
     ret = rpmjsRunFile(js, Ifn, Iargv, &result);
     switch (ret) {
     default:
-	rpmlog(RPMLOG_NOTICE, "%s: Unable to open' script '%s'! (%s)\n",
+    {	int code = gpsee_getExceptionExitCode(I->cx);
+	if (code >= 0) {	/** e.g. throw 6 */
+assert(I->grt->exitType == et_requested);
+assert(I->grt->exitCode == code);
+	    I->grt->exitType = et_requested;
+	    I->grt->exitCode = code;
+	} else
+	if (JS_IsExceptionPending(I->cx)) {
+assert(I->grt->exitType == et_exception);
+	    I->grt->exitType = et_exception;
+	    gpsee_reportUncaughtException(I->cx, JSVAL_NULL,
+                                        (gpsee_verbosity(0) >= GSR_FORCE_STACK_DUMP_VERBOSITY) ||
+                                        ((gpsee_verbosity(0) >= GPSEE_ERROR_OUTPUT_VERBOSITY) && isatty(STDERR_FILENO)));
+	} else {
+assert(I->grt->exitType == et_execFailure);
+	    I->grt->exitType = et_execFailure;
+	    rpmlog(RPMLOG_NOTICE, "%s: Unable to open' script '%s'! (%s)\n",
 		      __progname, Ifn, result);
+	}
 	result = _free(result);
-	ec = ((int)ret < 0 ? -ret : ret);
-	break;
+    }	break;
     case RPMRC_OK:
+#ifdef	NOTYET		/* XXX FIXME */
+assert(I->grt->exitType == et_finished);
+#endif
+	I->grt->exitType = et_finished;
 	/* XXX print result? */
-	ec = ret;
 	break;
     }
 
@@ -257,15 +349,26 @@ finish:
 #ifdef GPSEE_DEBUGGER
     gpsee_finiDebugger(jsdc);
 #endif
-    js = rpmjsFree(js);
+
+    /* XXX Map the exit code identically to gsr. */
+    ec = _gsrExitCode(js);
 
 exit:
+    js = rpmjsFree(js);
+
     Icode = _free(Icode);
     Ifn = _free(Ifn);
 
     optCon = rpmioFini(optCon);
 
     return ec;
+}
+
+int main(int argc, char *argv[])
+{
+  int ec = PR_Initialize(_main, argc, argv, 0);
+  (void) PR_Cleanup();
+  return ec;
 }
 
 #ifdef GPSEE_DEBUGGER
@@ -279,3 +382,5 @@ extern void __attribute__ ((noinline)) gpsee_breakpoint(void)
     return;
 }
 #endif
+
+
