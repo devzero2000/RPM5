1be09518a941cc5f88348bdbdabae23a1f727afc
devzero2000 <devzero2000>
2013-08-08 13:55:15 +0000

    debugedit.c:
    fix #opensusebz 818502

--- tools/debugedit.c
+++ tools/debugedit.c
@@ -252,6 +252,7 @@ static struct
 #define DEBUG_RANGES	10
 #define DEBUG_TYPES	11
 #define DEBUG_MACRO	12
+#define DEBUG_GDB_SCRIPT	13
     { ".debug_info", NULL, NULL, 0, 0, 0 },
     { ".debug_abbrev", NULL, NULL, 0, 0, 0 },
     { ".debug_line", NULL, NULL, 0, 0, 0 },
@@ -265,6 +266,7 @@ static struct
     { ".debug_ranges", NULL, NULL, 0, 0, 0 },
     { ".debug_types", NULL, NULL, 0, 0, 0 },
     { ".debug_macro", NULL, NULL, 0, 0, 0 },
+    { ".debug_gdb_scripts", NULL, NULL, 0, 0, 0 },
     { NULL, NULL, NULL, 0, 0, 0 }
   };
 
