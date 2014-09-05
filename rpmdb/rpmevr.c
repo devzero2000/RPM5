e978e31dbf074d0fd54a4ff31b84920c9471e76d
jbj <jbj>
2014-09-05 19:21:21 +0000

    - python: expose _invert_digits_alphas_comparison for rpm-python setting.

--- rpmdb/rpmevr.c
+++ rpmdb/rpmevr.c
@@ -57,9 +57,9 @@ EVR_t rpmEVRfree(EVR_t evr)
 /* XXX Force digits to beat alphas. See bugzilla #50977. */
 /*@unchecked@*/
 #if defined(RPM_VENDOR_MANDRIVA)  || defined(RPMVERCMP_DIGITS_BEAT_ALPHA) /* old-comparision-behaviour */
-static int _invert_digits_alphas_comparison = -1;
+int _invert_digits_alphas_comparison = -1;
 #else
-static int _invert_digits_alphas_comparison = 1;
+int _invert_digits_alphas_comparison = 1;
 #endif
 
 /* XXX Punctuation characters that are not treated as alphas */
