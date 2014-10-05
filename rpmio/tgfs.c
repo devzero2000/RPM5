607981d4e8421d3fc492bf621fc200fa4d2758ca
jbj <jbj>
2014-10-05 20:44:06 +0000

    - tgfs: merge mong-stat.c.

--- rpmio/tgfs.c
+++ rpmio/tgfs.c
@@ -30,6 +30,151 @@ static char * _gfs_uri =
 	"%{_gfs_scheme}://%{_gfs_host}/%{_gfs_db}";
 
 /*==============================================================*/
+
+#pragma pack(1)
+typedef struct
+{
+    uint32_t offset;
+    uint32_t slot;
+    char          category[24];
+    char          name[32];
+    char          description[64];
+} mongoc_counter_info_t;
+#pragma pack()
+
+BSON_STATIC_ASSERT(sizeof(mongoc_counter_info_t) == 128);
+
+#pragma pack(1)
+typedef struct
+{
+    uint32_t size;
+    uint32_t n_cpu;
+    uint32_t n_counters;
+    uint32_t infos_offset;
+    uint32_t values_offset;
+    uint8_t  padding[44];
+} mongoc_counters_t;
+#pragma pack()
+
+BSON_STATIC_ASSERT(sizeof(mongoc_counters_t) == 64);
+
+BSON_STATIC_ASSERT(sizeof(mongoc_counter_slots_t) == 64);
+
+static mongoc_counters_t *
+mongoc_counters_new_from_pid (unsigned pid)
+{
+    mongoc_counters_t *counters = NULL;
+    int32_t len;
+    size_t size;
+    void *mem;
+    char name[32];
+    int fdno = -1;
+
+    snprintf (name, sizeof name, "/mongoc-%u", pid);
+    name [sizeof name-1] = '\0';
+
+    fdno = shm_open(name, O_RDONLY, 0);
+    if (fdno == -1) {
+	perror("Failed to load shared memory segment");
+	goto exit;
+    }
+
+    if (pread (fdno, &len, 4, 0) != 4) {
+	perror("Failed to load shared memory segment");
+	goto exit;
+    }
+
+    if (len == 0) {
+	perror("Shared memory area is not yet initialized by owning process.");
+	goto exit;
+    }
+
+    size = len;
+
+    mem = mmap(NULL, size, PROT_READ, MAP_SHARED, fdno, 0);
+    if (mem == MAP_FAILED) {
+	fprintf(stderr, "Failed to mmap shared memory segment of size: %u",
+		(unsigned)size);
+	goto exit;
+    }
+
+    counters = (mongoc_counters_t *)mem;
+    if (counters->size != (size_t)len) {
+	perror("Corrupted shared memory segment.");
+	counters = NULL;
+	goto exit;
+    }
+
+exit:
+    if (fdno > 0)
+	close(fdno);
+
+   return counters;
+}
+
+static void
+mongoc_counters_destroy (mongoc_counters_t *counters)
+{
+    BSON_ASSERT(counters);
+    munmap((void *)counters, counters->size);
+}
+
+static mongoc_counter_info_t *
+mongoc_counters_get_infos (mongoc_counters_t *counters, uint32_t *n_infos)
+{
+    mongoc_counter_info_t *info;
+    char *base = (char *)counters;
+
+    BSON_ASSERT(counters);
+    BSON_ASSERT(n_infos);
+
+    info = (mongoc_counter_info_t *)(base + counters->infos_offset);
+    *n_infos = counters->n_counters;
+
+    return info;
+}
+
+static int64_t
+mongoc_counters_get_value (mongoc_counters_t *counters,
+		mongoc_counter_info_t *info, mongoc_counter_t *counter)
+{
+    int64_t value = 0;
+    unsigned i;
+
+    for (i = 0; i < counters->n_cpu; i++)
+	value += counter->cpus[i].slots[info->slot];
+
+    return value;
+}
+
+static void
+mongoc_counters_print_info (mongoc_counters_t *counters,
+		mongoc_counter_info_t *info, FILE *file)
+{
+    mongoc_counter_t ctr;
+    int64_t value;
+
+    BSON_ASSERT (info);
+    BSON_ASSERT (file);
+    BSON_ASSERT ((info->offset & 0x7) == 0);
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcast-align"
+#endif
+    ctr.cpus = (mongoc_counter_slots_t *)(((char *)counters)+ info->offset);
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+    value = mongoc_counters_get_value(counters, info, &ctr);
+
+    fprintf(file, "%24s : %-24s : %-50s : %lld\n",
+		info->category, info->name, info->description,
+		(long long)value);
+}
+
+/*==============================================================*/
 static int doit(rpmgfs gfs, int ac, ARGV_t av)
 {
     int rc = 1;		/* assume failure */
@@ -59,6 +204,32 @@ static int doit(rpmgfs gfs, int ac, ARGV_t av)
 	rc = rpmgfsList(gfs);
     } else if (strcmp(av[0], "dump") == 0) {
 	rc = rpmgfsDump(gfs);
+    } else if (strcmp(av[0], "stat") == 0) {
+	mongoc_counter_info_t * infos;
+	mongoc_counters_t * counters;
+	uint32_t n_counters = 0;
+	unsigned i;
+	int pid;
+
+	if (ac != 2) {
+	    fprintf(stderr, "usage: - %s stat PID\n", __progname);
+	    goto exit;
+	}
+
+	pid = strtol(av[1], NULL, 10);
+
+	counters = mongoc_counters_new_from_pid (pid);
+	if (counters == NULL) {
+	    fprintf (stderr, "%s: Failed to load shared memory for pid %u.\n",
+			__progname, pid);
+	    goto exit;
+	}
+
+	infos = mongoc_counters_get_infos (counters, &n_counters);
+	for (i = 0; i < n_counters; i++)
+	    mongoc_counters_print_info (counters, &infos[i], stdout);
+	mongoc_counters_destroy (counters);
+
     } else {
 	fprintf(stderr, "Unknown command: %s\n", av[0]);
 	goto exit;
