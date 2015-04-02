5267b7101e07a2d9a1ea2c5df102c3f044bf8d54
devzero2000 <devzero2000>
2015-04-02 12:57:10 +0000

    find-provides.php: use the $( ... \) construct for command substitution
    Prefer the $( ... ) construct for command
    substitution instead of using the back-quotes, or grave accents (`..`).
    
    The backquoted form is the historical method for command substitution,
    and is supported by POSIX. However, all but the simplest uses become
    complicated quickly. In particular, embedded command substitutions
    and/or the use of double quotes require careful escaping with the backslash
    character. Because of this the POSIX shell adopted the $(…) feature from
    the Korn shell.
    
    The patch was generated by the simple script
    
    for _f in $(find . -type f -exec file {} \; | grep -i shell | awk -F: '{ print $1 }')
    do
      perl -i -pe 'BEGIN{undef $/;} s@`(.*)`@\$($1)@smg' ${_f}
    done

--- scripts/find-provides.php
+++ scripts/find-provides.php
@@ -4,11 +4,11 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 
-filelist=`echo $@`
+filelist=$(echo $@)
 for i in $filelist; do
-	i=`echo $i | grep "\.php$"`
+	i=$(echo $i | grep "\.php$")
 	if [ -n "$i" ]; then
-		j=`cat $i |egrep  -i "^Class" |cut -f 2 -d " "| tr -d "\r"`
+		j=$(cat $i |egrep  -i "^Class" |cut -f 2 -d " "| tr -d "\r")
 		if [ -n "$j" ]; then
 			for p in $j; do
 				echo "pear($p)"
