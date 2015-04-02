8411c408be2bc8b3302aec0fa5fd7adef810fb47
devzero2000 <devzero2000>
2015-04-02 12:57:08 +0000

    find-prov.pl: use the $( ... \) construct for command substitution
    Prefer the $( ... ) construct for command
    substitution instead of using the back-quotes, or grave accents (`..`).
    
    The backquoted form is the historical method for command substitution,
    and is supported by POSIX. However, all but the simplest uses become
    complicated quickly. In particular, embedded command substitutions
    and/or the use of double quotes require careful escaping with the backslash
    character. Because of this the POSIX shell adopted the $(…) feature from
    the Korn shell.
    
    The patch was generated by the simple script
    
    for _f in $(find . -type f -exec file {} \; | grep -i shell | awk -F: '{ print $1 }')
    do
      perl -i -pe 'BEGIN{undef $/;} s@`(.*)`@\$($1)@smg' ${_f}
    done

--- scripts/find-prov.pl
+++ scripts/find-prov.pl
@@ -64,13 +64,13 @@ done
 
 
 
-for file in `cat -`
+for file in $(cat -)
 do
 
 # this section is for processing based on the interpreter specified in
 # the '#!' line.
 
-case `get_magic $file` in 
+case $(get_magic $file) in 
 
 bash)
     print_deps --identifier executable $file
@@ -201,7 +201,7 @@ case $file in
     # using file endings like the browsers do.
     # precise globbing is hard so I use egrep instead of the case statement.
 
-hfile=`basename $file | egrep '\.((cgi)|(ps)|(pdf)|(png)|(jpg)|(gif)|(tiff)|(tif)|(xbm)|(html)|(htm)|(shtml)|(jhtml))$'`;
+hfile=$(basename $file | egrep '\.((cgi)|(ps)|(pdf)|(png)|(jpg)|(gif)|(tiff)|(tif)|(xbm)|(html)|(htm)|(shtml)|(jhtml))$');
 
 	if [ "${hfile}" != "" ]
 	then
