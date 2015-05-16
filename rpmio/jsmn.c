ccf2ac2bef7574e1c4aaf56f4d29477a5de2e778
jbj <jbj>
2015-05-16 23:31:17 +0000

    - resurrect the github testcase.

--- rpmio/jsmn.c
+++ rpmio/jsmn.c
@@ -1,5 +1,27 @@
 #define	JSMN_PARENT_LINKS	1
 /*==============================================================*/
+/*
+ * Copyright (c) 2010 Serge A. Zaitsev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/*==============================================================*/
 /* --- jsmn.h */
 
 #ifdef __cplusplus
@@ -71,9 +93,13 @@ extern "C" {
 }
 #endif
 
-/*==============================================================*//* --- jsmn.c *//**
+/*==============================================================*/
+/* --- jsmn.c */
+
+/**
  * Allocates a fresh unused token from the token pull.
- */ static jsmntok_t *jsmn_alloc_token(jsmn_parser * parser,
+ */
+static jsmntok_t *jsmn_alloc_token(jsmn_parser * parser,
 				       jsmntok_t * tokens,
 				       size_t num_tokens)
 {
